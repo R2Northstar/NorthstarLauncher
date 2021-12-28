9b514e53388a22458ef4c1bbe6ad975f49548463
GeckoEidechse <gecko.eidechse+git@pm.me>
2021-12-29 00:42:44 +0100

    Make timestamp year-month-day instead of day-month-year

--- NorthstarDedicatedTest/logging.cpp
+++ NorthstarDedicatedTest/logging.cpp
@@ -176,12 +176,12 @@ void InitialiseLogging()
 	spdlog::flush_on(spdlog::level::info);
 
 	// log file stuff
-	// generate log file, format should be nslog%d-%m-%Y %H-%M-%S.txt in gamedir/R2Northstar/logs
+	// generate log file, format should be nslog%Y-%m-%d %H-%M-%S.txt in gamedir/R2Northstar/logs
 	// todo: might be good to delete logs that are too old
 	time_t time = std::time(nullptr);
 	tm currentTime = *std::localtime(&time);
 	std::stringstream stream;
-	stream << std::put_time(&currentTime, "R2Northstar/logs/nslog%d-%m-%Y %H-%M-%S.txt");
+	stream << std::put_time(&currentTime, "R2Northstar/logs/nslog%Y-%m-%d %H-%M-%S.txt");
 
 	// create logger
 	spdlog::default_logger()->sinks().push_back(std::make_shared<spdlog::sinks::basic_file_sink_mt>(stream.str(), false));
