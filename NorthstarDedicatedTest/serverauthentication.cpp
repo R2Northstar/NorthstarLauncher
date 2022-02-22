81c5ba3282b4771684eff8867e2fc1a6e66ebc77
Barnaby <22575741+barnabwhy@users.noreply.github.com>
2022-02-22 18:49:31 +0000

    Format

--- NorthstarDedicatedTest/serverauthentication.cpp
+++ NorthstarDedicatedTest/serverauthentication.cpp
@@ -150,7 +150,8 @@ char* ServerAuthenticationManager::VerifyPlayerName(void* player, char* authToke
 
 	bool nameAccepted = (!*authData.username || name == "" || !strcmp(name, authData.username));
 
-	if (!nameAccepted && g_MasterServerManager->m_bRequireClientAuth && !CVar_ns_auth_allow_insecure->m_nValue) {
+	if (!nameAccepted && g_MasterServerManager->m_bRequireClientAuth && !CVar_ns_auth_allow_insecure->m_nValue)
+	{
 		strcpy(name, authData.username);
 	}
 	return name;
