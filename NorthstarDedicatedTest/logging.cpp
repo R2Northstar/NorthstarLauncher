65819becab4a0e1f44e3bb47b86348d7afa27288
Barnaby <22575741+barnabwhy@users.noreply.github.com>
2022-03-06 19:38:31 +0000

    on crash hide game and show console

--- NorthstarDedicatedTest/logging.cpp
+++ NorthstarDedicatedTest/logging.cpp
@@ -195,6 +195,15 @@ long __stdcall ExceptionFilter(EXCEPTION_POINTERS* exceptionInfo)
 			spdlog::error("Failed to write minidump file {}!", stream.str());
 
 		if (!IsDedicated())
+		{
+			PostMessage(*g_gameHwnd, WM_SYSCOMMAND, SC_MINIMIZE, 0);
+			ShowWindow(*g_gameHwnd, SW_HIDE);
+			ShowWindow(GetConsoleWindow(), SW_SHOW);
+			SetForegroundWindow(GetConsoleWindow());
+			SetFocus(GetConsoleWindow());
+			SetActiveWindow(GetConsoleWindow());
+			ShowWindow(GetConsoleWindow(), SW_RESTORE);
+
 			MessageBoxA(
 				0, "Northstar has crashed! Crash info can be found in R2Northstar/logs", "Northstar has crashed!", MB_ICONERROR | MB_OK);
 	}
