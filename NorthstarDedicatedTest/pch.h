75abdd9c457fc0bf1a187119b7e53ce39bdc3513
KittenPopo <Pokeberry123@gmail.com>
2022-04-09 13:21:50 -0700

    Force NS_DEBUG if _DEBUG

--- NorthstarDedicatedTest/pch.h
+++ NorthstarDedicatedTest/pch.h
@@ -2,6 +2,10 @@
 #ifndef PCH_H
 #define PCH_H
 
+#ifdef _DEBUG
+#define NSDEBUG
+#endif
+
 #define WIN32_LEAN_AND_MEAN
 #define _CRT_SECURE_NO_WARNINGS
 #define RAPIDJSON_NOMEMBERITERATORCLASS // need this for rapidjson
