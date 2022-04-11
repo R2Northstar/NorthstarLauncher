e75b6e2ae566232a0b2d3a9ee6cdf8f49a7e9631
KittenPopo <Pokeberry123@gmail.com>
2022-04-11 14:47:42 -0700

    Fixed occasional print bug by not double-initializing console

--- NorthstarDedicatedTest/logging.cpp
+++ NorthstarDedicatedTest/logging.cpp
@@ -287,11 +287,6 @@ void InitialiseLogging()
 {
 	hExceptionFilter = AddVectoredExceptionHandler(TRUE, ExceptionFilter);
 
-	AllocConsole();
-	freopen("CONOUT$", "w", stdout);
-	freopen("CONOUT$", "w", stderr);
-	spdlog::default_logger()->set_pattern("[%H:%M:%S] [%l] %v");
-
 	SetConsoleCtrlHandler(ConsoleHandlerRoutine, true);
 }
 
