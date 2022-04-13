db6ac1da67083ce1c138ee56549d3e984a553158
GeckoEidechse <gecko.eidechse+git@pm.me>
2022-04-13 22:55:09 +0200

    Fix formatting to stop clang-format failing in CI

--- NorthstarDedicatedTest/audio.cpp
+++ NorthstarDedicatedTest/audio.cpp
@@ -214,7 +214,7 @@ EventOverrideData::EventOverrideData(const std::string& data, const fs::path& pa
 			// thread off the file read
 			// should we spawn one thread per read? or should there be a cap to the number of reads at once?
 			std::thread readThread(
-				[pathString, fileSize, data] 
+				[pathString, fileSize, data]
 				{
 					std::shared_lock lock(g_CustomAudioManager.m_loadingMutex);
 					std::basic_ifstream<uint8_t> wavStream(pathString, std::ios::binary);
