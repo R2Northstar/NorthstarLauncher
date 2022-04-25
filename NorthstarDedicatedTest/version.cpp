e9132166fdd964fe5a5f50588471429ba7592b61
GeckoEidechse <gecko.eidechse+git@pm.me>
2022-04-25 23:56:33 +0200

    Set Northstar version in separate header file

--- NorthstarDedicatedTest/version.cpp
+++ NorthstarDedicatedTest/version.cpp
@@ -1,13 +1,9 @@
 #include "version.h"
 #include "pch.h"
+#include "ns_version.h"
 
 char version[16];
 char NSUserAgent[32];
-
-// Turning off clang-format here so it doesn't mess with style as it needs to be this way for CI
-// clang-format off
-#define NORTHSTAR_VERSION 0,0,0,1
-// clang-format on
 
 void InitialiseVersion()
 {
