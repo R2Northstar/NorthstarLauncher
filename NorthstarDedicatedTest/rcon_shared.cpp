af9e2fe286e97140c40b2461bbd5d131125ac768
Kawe Mazidjatari <48657826+Mauler125@users.noreply.github.com>
2022-05-07 11:47:16 +0200

    RCON system improvements
    * Buffers are now send properly from the game client and netconsole client (running scripts and executing command's/convar's now work properly).
    * Removed 'PASS' string check on server and only rely on 'SERVERDATA_REQUEST_AUTH' enum for auth queries (should make heavy abuse even harder).

--- NorthstarDedicatedTest/rcon_shared.cpp
+++ NorthstarDedicatedTest/rcon_shared.cpp
@@ -30,72 +30,44 @@ void _RCON_CmdQuery_f(const CCommand& args)
 		return;
 	}
 
-	switch (args.ArgC())
+	if (args.ArgC() < 2)
 	{
-	case 0:
-	case 1:
-	{
-		if (g_pRConClient->IsInitialized() && !g_pRConClient->IsConnected() && strlen(CVar_rcon_address->GetString()) > 0)
+		if (g_pRConClient->IsInitialized() && !g_pRConClient->IsConnected() && strlen(CVar_rcon_password->GetString()) > 0)
 		{
 			g_pRConClient->Connect();
 		}
-		break;
 	}
-	case 2:
+	else
 	{
 		if (!g_pRConClient->IsInitialized())
 		{
 			spdlog::warn("Failed to issue command to RCON server: uninitialized\n");
-			break;
+			return;
 		}
 		else if (g_pRConClient->IsConnected())
 		{
 			if (strcmp(args.Arg(1), "PASS") == 0) // Auth with RCON server using rcon_password ConVar value.
 			{
 				std::string svCmdQuery =
-					g_pRConClient->Serialize(args[1], CVar_rcon_password->GetString(), cl_rcon::request_t::SERVERDATA_REQUEST_EXECCOMMAND);
+					g_pRConClient->Serialize(CVar_rcon_password->GetString(), "", cl_rcon::request_t::SERVERDATA_REQUEST_AUTH);
 				g_pRConClient->Send(svCmdQuery);
-				break;
+				return;
 			}
 			else if (strcmp(args.Arg(1), "disconnect") == 0) // Disconnect from RCON server.
 			{
 				g_pRConClient->Disconnect();
-				break;
+				return;
 			}
 
-			std::string svCmdQuery = g_pRConClient->Serialize(args.Arg(1), "", cl_rcon::request_t::SERVERDATA_REQUEST_EXECCOMMAND);
+			std::string svCmdQuery = g_pRConClient->Serialize(args.ArgS(), "", cl_rcon::request_t::SERVERDATA_REQUEST_EXECCOMMAND);
 			g_pRConClient->Send(svCmdQuery);
-			break;
+			return;
 		}
 		else
 		{
 			spdlog::warn("Failed to issue command to RCON server: unconnected\n");
-			break;
+			return;
 		}
-		break;
-	}
-	case 3:
-	{
-		if (g_pRConClient->IsConnected())
-		{
-			if (strcmp(args[1], "PASS") == 0) // Auth with RCON server.
-			{
-				std::string svCmdQuery = g_pRConClient->Serialize(args.Arg(1), args.Arg(2), cl_rcon::request_t::SERVERDATA_REQUEST_AUTH);
-				g_pRConClient->Send(svCmdQuery);
-				break;
-			}
-
-			std::string svCmdQuery = g_pRConClient->Serialize(args.Arg(1), args.Arg(2), cl_rcon::request_t::SERVERDATA_REQUEST_SETVALUE);
-			g_pRConClient->Send(svCmdQuery);
-			break;
-		}
-		else
-		{
-			spdlog::warn("Failed to issue command to RCON server: unconnected\n");
-			break;
-		}
-		break;
-	}
 	}
 }
 
