07e90754ab86f7d56df2f813ceb93de12d4aa0d5
Emma Miler <27428383+emma-miler@users.noreply.github.com>
2022-05-13 01:53:22 +0200

    Block `emit` command when sv_cheats is disabled (#170)

--- NorthstarDedicatedTest/dllmain.cpp
+++ NorthstarDedicatedTest/dllmain.cpp
@@ -49,6 +49,7 @@
 #include "rapidjson/writer.h"
 #include "rapidjson/error/en.h"
 #include "ExploitFixes.h"
+#include "emit_blocker.h"
 
 typedef void (*initPluginFuncPtr)(void* getPluginObject);
 
@@ -283,6 +284,7 @@ bool InitialiseNorthstar()
 
 	// activate exploit fixes
 	AddDllLoadCallback("server.dll", ExploitFixes::LoadCallback);
+	AddDllLoadCallback("server.dll", InitialiseServerEmit_Blocker);
 
 	// run callbacks for any libraries that are already loaded by now
 	CallAllPendingDLLLoadCallbacks();
