06e2bc153a6a81b15cdb04893dbf8ed3ccda8a9d
KittenPopo <Pokeberry123@gmail.com>
2022-05-31 15:08:49 -0700

    Reimplement client chat hooks (?)

--- NorthstarDedicatedTest/clientchathooks.cpp
+++ NorthstarDedicatedTest/clientchathooks.cpp
@@ -54,8 +54,42 @@ static void CHudChat__AddGameLineHook(void* self, const char* message, int inbox
 	}
 }
 
+// void NSChatWrite( int context, string str )
+static SQRESULT SQ_ChatWrite(void* sqvm)
+{
+	int context = ClientSq_getinteger(sqvm, 1);
+	const char* str = ClientSq_getstring(sqvm, 2);
+
+	LocalChatWriter((LocalChatWriter::Context)context).Write(str);
+	return SQRESULT_NOTNULL;
+}
+
+// void NSChatWriteRaw( int context, string str )
+static SQRESULT SQ_ChatWriteRaw(void* sqvm)
+{
+	int context = ClientSq_getinteger(sqvm, 1);
+	const char* str = ClientSq_getstring(sqvm, 2);
+
+	LocalChatWriter((LocalChatWriter::Context)context).InsertText(str);
+	return SQRESULT_NOTNULL;
+}
+
+// void NSChatWriteLine( int context, string str )
+static SQRESULT SQ_ChatWriteLine(void* sqvm)
+{
+	int context = ClientSq_getinteger(sqvm, 1);
+	const char* str = ClientSq_getstring(sqvm, 2);
+
+	LocalChatWriter((LocalChatWriter::Context)context).WriteLine(str);
+	return SQRESULT_NOTNULL;
+}
+
 void InitialiseClientChatHooks(HMODULE baseAddress)
 {
 	HookEnabler hook;
 	ENABLER_CREATEHOOK(hook, (char*)baseAddress + 0x22E580, &CHudChat__AddGameLineHook, reinterpret_cast<LPVOID*>(&CHudChat__AddGameLine));
+
+	g_ClientSquirrelManager->AddFuncRegistration("void", "NSChatWrite", "int context, string text", "", SQ_ChatWrite);
+	g_ClientSquirrelManager->AddFuncRegistration("void", "NSChatWriteRaw", "int context, string text", "", SQ_ChatWriteRaw);
+	g_ClientSquirrelManager->AddFuncRegistration("void", "NSChatWriteLine", "int context, string text", "", SQ_ChatWriteLine);
 }
