0588df6fe4b76b08d55b2cac73303bcadb9c8162
Emma Miler <27428383+emma-miler@users.noreply.github.com>
2022-06-14 15:02:09 +0200

    Fix bug where emit wouldnt be blocked if uppercase
    temp fix while we wait for #178 to not suck i guess

--- NorthstarDedicatedTest/emit_blocker.cpp
+++ NorthstarDedicatedTest/emit_blocker.cpp
@@ -6,10 +6,26 @@ ConVar* sv_cheats;
 typedef char(__fastcall* function_containing_emit_t)(uint64_t a1, uint64_t a2);
 function_containing_emit_t function_containing_emit;
 
+// https://stackoverflow.com/questions/5661642/c-case-insensitive-first-n-characters-string-comparison
+bool strncmp_insensitive(char const* s1, char const* s2, int n)
+{
+	for (int i = 0; i < n; ++i)
+	{
+		unsigned char c1 = static_cast<unsigned char>(s1[i]);
+		unsigned char c2 = static_cast<unsigned char>(s2[i]);
+		if (tolower(c1) != tolower(c2))
+			return false;
+		if (c1 == '\0' || c2 == '\0')
+			break;
+	}
+	return true;
+}
+
 char function_containing_emit_hook(uint64_t unknown_value, uint64_t command_ptr)
 {
 	char* command_string = *(char**)(command_ptr + 1040); // From decompile
-	if (!sv_cheats->m_Value.m_nValue && !strncmp(command_string, "emit", 5))
+	
+	if (!sv_cheats->m_Value.m_nValue && strncmp_insensitive(command_string, "emit", 5))
 	{
 		spdlog::info("Blocking command \"emit\" because sv_cheats was 0");
 		return 1;
