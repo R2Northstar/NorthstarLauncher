4697af2de45d360ded9e2be27413857fffeb8c66
KittenPopo <Pokeberry123@gmail.com>
2022-06-17 15:50:21 -0700

    Add detailed comments to all NSMem functions

--- NorthstarDedicatedTest/NSMem.h
+++ NorthstarDedicatedTest/NSMem.h
@@ -6,20 +6,64 @@
 #pragma region Pattern Scanning
 namespace NSMem
 {
+	// Returns an array of byte values from a hexadecimal string
+	// A byte value will be -1 where the input string contained a wildcard ('?')
 	std::vector<int> StringToHexBytes(const char* str);
 
+	/// <summary>
+	/// Find a byte pattern in a module (raw function alternative)
+	/// </summary>
+	/// <param name="module">Base address of the module (handle)</param>
+	/// <param name="pattern">Array of byte values to search for (-1 is for wildcard)</param>
+	/// <param name="patternSize">Size of the byte pattern array</param>
+	/// <param name="offset">Amount to shift output pointer address by</param>
+	/// <returns>Address of found result (+ offset), or NULL if not found</returns>
 	void* PatternScan(void* module, const int* pattern, int patternSize, int offset);
 
+	/// <summary>
+	/// Find a byte pattern in a module
+	/// </summary>
+	/// <param name="moduleName">Name of module, will be passed to GetModuleHandleA()</param>
+	/// <param name="pattern">The byte pattern to search for as a hex string (see StringToHexBytes())</param>
+	/// <param name="offset">Amount to shift output pointer address by</param>
+	/// <returns>Address of found result (+ offset), or NULL if not found</returns>
 	void* PatternScan(const char* moduleName, const char* pattern, int offset = 0);
 
+	/// <summary>
+	/// Byte patch memory at a given address
+	/// </summary>
+	/// <param name="address">Address to patch at</param>
+	/// <param name="vals">Array of byte values to replace with</param>
+	/// <param name="size">Size of byte value array</param>
 	void BytePatch(uintptr_t address, const BYTE* vals, int size);
 
+	/// <summary>
+	/// Byte patch memory at a given address
+	/// </summary>
+	/// <param name="address">Address to patch at</param>
+	/// <param name="vals">Initializer list of byte values to replace with</param>
 	void BytePatch(uintptr_t address, std::initializer_list<BYTE> vals);
 
+	/// <summary>
+	/// Byte patch memory at a given address
+	/// </summary>
+	/// <param name="address">Address to patch at</param>
+	/// <param name="bytesStr">Hex string of bytes to patch with (see StringToHexBytes())</param>
 	void BytePatch(uintptr_t address, const char* bytesStr);
 
+	/// <summary>
+	/// Byte patch instructions at a given address to NOP opcodes, making them do nothing
+	/// Functionally identical to using BytePatch(address, { 0x90, 0x90, 0x90, ... })
+	/// </summary>
+	/// <param name="address">Address to patch at</param>
+	/// <param name="size">Amount of bytes to patch</param>
 	void NOP(uintptr_t address, int size);
 
+	/// <summary>
+	/// Checks if we are able to read memory from a given address
+	/// </summary>
+	/// <param name="ptr">Address to check (start of region)</param>
+	/// <param name="size">Size of the region to check</param>
 	bool IsMemoryReadable(void* ptr, size_t size);
 } // namespace NSMem
 
