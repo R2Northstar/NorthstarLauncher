28ed714fd2e26498da6be26dc6199974adfe1b23
EladNLG <44613424+EladNLG@users.noreply.github.com>
2022-07-21 16:59:21 +0300

    Add ServerSq versions of the IO funcs

--- NorthstarDedicatedTest/modsavefiles.cpp
+++ NorthstarDedicatedTest/modsavefiles.cpp
@@ -109,6 +109,103 @@ SQRESULT ClientSq_LoadJSON(void* sqvm)
 	return SQRESULT_ERROR;
 }
 
+SQRESULT ServerSq_SaveJSON(void* sqvm)
+{
+	std::string modName = ServerSq_getstring(sqvm, 1);
+	std::string file = ServerSq_getstring(sqvm, 2);
+	std::string content = EncodeJSON(sqvm);
+	if (content.length() > CHARACTER_LIMIT)
+	{
+		ServerSq_pusherror(
+			sqvm,
+			fmt::format(
+				"File content length over character limit ({})! Reduce the table's contents, or use multiple files!",
+				CHARACTER_LIMIT,
+				modName)
+				.c_str());
+		return SQRESULT_ERROR;
+	}
+	if (ContainsNonASCIIChars(content))
+	{
+		ServerSq_pusherror(
+			sqvm, fmt::format("File contents may not contain non-ASCII characters! Make sure your strings are valid!", modName).c_str());
+		return SQRESULT_ERROR;
+	}
+	for (Mod& mod : g_ModManager->m_loadedMods)
+		if (modName == mod.Name)
+		{
+			if (!mod.Enabled)
+			{
+				ServerSq_pusherror(sqvm, fmt::format("Mod with name {} was not found!", modName).c_str());
+				return SQRESULT_ERROR;
+			}
+			for (std::string fileName : mod.SaveFiles)
+			{
+				if (fileName == file)
+				{
+					fs::create_directories(fs::path(GetNorthstarPrefix()) / "saveData");
+					std::ofstream fileStr(fs::path(GetNorthstarPrefix()) / "saveData" / (file + ".json"));
+					if (fileStr.fail())
+					{
+						ServerSq_pusherror(
+							sqvm, fmt::format("There was an error opening/creating file {} (Is the file name valid?)", file).c_str());
+						return SQRESULT_ERROR;
+					}
+					fileStr.write(content.c_str(), content.length());
+					fileStr.close();
+					return SQRESULT_NULL;
+				}
+			}
+		}
+	ServerSq_pusherror(sqvm, fmt::format("Mod with name {} was not found!", file).c_str());
+	return SQRESULT_ERROR;
+}
+
+SQRESULT ServerSq_LoadJSON(void* sqvm)
+{
+	std::string modName = ServerSq_getstring(sqvm, 1);
+	std::string file = ServerSq_getstring(sqvm, 2);
+	for (Mod& mod : g_ModManager->m_loadedMods)
+		if (modName == mod.Name)
+		{
+			if (!mod.Enabled)
+			{
+				ServerSq_pusherror(sqvm, fmt::format("Mod with name {} was not found!", modName).c_str());
+				return SQRESULT_ERROR;
+			}
+			for (std::string fileName : mod.SaveFiles)
+			{
+				if (fileName == file)
+				{
+					std::ifstream fileStr(fs::path(GetNorthstarPrefix()) / "saveData" / (file + ".json"));
+					if (fileStr.fail())
+					{
+						ServerSq_newTable(sqvm);
+						return SQRESULT_NOTNULL;
+					}
+					std::stringstream jsonStringStream;
+					while (fileStr.peek() != EOF)
+						jsonStringStream << (char)fileStr.get();
+					rapidjson_document doc;
+					doc.Parse(jsonStringStream.str().c_str());
+					if (doc.HasParseError())
+					{
+						ServerSq_newTable(sqvm);
+						return SQRESULT_NOTNULL;
+					}
+					// spdlog::warn("Passing to DecodeJSON... {}", doc["test"].GetString());
+					ServerSq_DecodeJSON_Table(
+						sqvm, (rapidjson::GenericValue<rapidjson::UTF8<char>, rapidjson::MemoryPoolAllocator<SourceAllocator>>*)&doc);
+					return SQRESULT_NOTNULL;
+				}
+			}
+			ServerSq_pusherror(sqvm, fmt::format("File with name {} was not found!", file).c_str());
+			return SQRESULT_ERROR;
+		}
+	ServerSq_pusherror(sqvm, fmt::format("Mod with name {} was not found!", modName).c_str());
+	return SQRESULT_ERROR;
+}
+
 std::string EncodeJSON(void* sqvm) {
 	rapidjson_document doc;
 	doc.SetObject();
@@ -133,7 +230,8 @@ void InitialiseClientSaveFiles(HMODULE baseAddress)
 
 void InitialiseServerSaveFiles(HMODULE baseAddress) 
 {
-
+	g_ServerSquirrelManager->AddFuncRegistration("table", "NSLoadModFile", "string mod, string file", "", ServerSq_LoadJSON);
+	g_ServerSquirrelManager->AddFuncRegistration("void", "NSSaveModFile", "string mod, string file, table data", "", ServerSq_SaveJSON);
 }
 
 bool ContainsNonASCIIChars(std::string str) 
