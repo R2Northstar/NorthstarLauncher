812893d8219daa60f5b5b7fd22cbd6b175603399
Jack <66967891+ASpoonPlaysGames@users.noreply.github.com>
2022-08-23 22:50:21 +0100

    Add log colours because they are pretty (#237)
    * log colours :)
    
    * commend the freopen stuff (it causes problems)
    
    * formatting
    
    * add explanatory comment

--- NorthstarDedicatedTest/logging.cpp
+++ NorthstarDedicatedTest/logging.cpp
@@ -234,9 +234,12 @@ void InitialiseLogging()
 	hExceptionFilter = AddVectoredExceptionHandler(TRUE, ExceptionFilter);
 
 	AllocConsole();
-	freopen("CONOUT$", "w", stdout);
-	freopen("CONOUT$", "w", stderr);
-	spdlog::default_logger()->set_pattern("[%H:%M:%S] [%l] %v");
+	// these two lines are responsible for stuff to not show up in the console sometimes, from talking about it on discord
+	// apparently they were meant to make logging work when using -northstar, however from testing it seems that it doesnt
+	// work regardless of these two lines
+	// freopen("CONOUT$", "w", stdout);
+	// freopen("CONOUT$", "w", stderr);
+	spdlog::default_logger()->set_pattern("[%H:%M:%S] [%^%l%$] %v");
 
 	SetConsoleCtrlHandler(ConsoleHandlerRoutine, true);
 }
