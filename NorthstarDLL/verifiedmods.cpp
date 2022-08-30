114653052972383f9b154d96c581a2b90c483c87
Alystrasz <contact@remyraes.com>
2022-08-30 08:31:34 +0200

    feat: add verifiedmods.cpp skeleton

---
+++ NorthstarDLL/verifiedmods.cpp
@@ -0,0 +1,17 @@
+#include "pch.h"
+
+void _FetchVerifiedModsList() {
+
+}
+
+char* GetVerifiedModsList() {
+	// TODO return list if it already has been parsed
+	// TODO else fetch list and return it
+}
+
+void DownloadMod(char* dependencyString) {
+	// TODO check if mod is already present
+	// TODO check if mod is verified (throw if not)
+	// TODO download zip in temporary folder
+	// TODO move mod to mods/ folder
+}
