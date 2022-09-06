ee0715d8bf928b3e7905f8b07587ea615b2953c7
Alystrasz <contact@remyraes.com>
2022-09-06 18:32:52 +0200

    refactor: remove unused prototypes from module header

--- NorthstarDLL/verifiedmods.h
+++ NorthstarDLL/verifiedmods.h
@@ -1,7 +1,4 @@
 #pragma once
 #include "memalloc.h"
 
-void _FetchVerifiedModsList();
-rapidjson_document GetVerifiedModsList();
-void DownloadMod(char* dependencyString);
 void InitialiseVerifiedModsScripts(HMODULE baseAddress);
