d7d92226bc13710f2387492d893187faf8da738b
BobTheBob9 <for.oliver.kirkham@gmail.com>
2022-10-08 01:44:02 +0100

    fixup command flags better and reformat

--- NorthstarDLL/host.cpp
+++ NorthstarDLL/host.cpp
@@ -3,6 +3,7 @@
 #include "modmanager.h"
 #include "printcommand.h"
 #include "printmaps.h"
+#include "misccommands.h"
 #include "r2engine.h"
 #include "tier0.h"
 
@@ -16,50 +17,7 @@ void, __fastcall, (bool bDedicated))
 	spdlog::info("Host_Init()");
 	Host_Init(bDedicated);
 
-    if (Tier0::CommandLine()->CheckParm("-allowdevcvars"))
-    {
-		// get all mod convars
-		std::vector<std::string> vModConvarNames;
-		for (auto& mod : g_pModManager->m_LoadedMods)
-			for (auto& cvar : mod.ConVars)
-				vModConvarNames.push_back(cvar->Name);
-
-	    // strip hidden and devonly cvar flags
-	    int iNumCvarsAltered = 0;
-	    for (auto& pair : R2::g_pCVar->DumpToMap())
-	    {
-	    	// don't remove from mod cvars
-	    	if (std::find(vModConvarNames.begin(), vModConvarNames.end(), pair.second->m_pszName) != vModConvarNames.end())
-	    		continue;
-
-	    	// strip flags
-	    	int flags = pair.second->GetFlags();
-	    	if (flags & FCVAR_DEVELOPMENTONLY)
-	    	{
-	    		flags &= ~FCVAR_DEVELOPMENTONLY;
-	    		iNumCvarsAltered++;
-	    	}
-
-	    	if (flags & FCVAR_HIDDEN)
-	    	{
-	    		flags &= ~FCVAR_HIDDEN;
-	    		iNumCvarsAltered++;
-	    	}
-
-	    	pair.second->m_nFlags = flags;
-	    }
-
-		spdlog::info("Removed {} hidden/devonly cvar flags", iNumCvarsAltered);
-    }
-
-	// make servers able to run disconnect on clients
-	R2::g_pCVar->FindCommand("disconnect")->m_nFlags |= FCVAR_SERVER_CAN_EXECUTE;
-
-	R2::g_pCVar->FindCommand("migrateme")->m_nFlags &= ~FCVAR_SERVER_CAN_EXECUTE;
-
-	// make clients able to run status and ping
-	R2::g_pCVar->FindCommand("status")->m_nFlags |= FCVAR_GAMEDLL_FOR_REMOTE_CLIENTS;
-	R2::g_pCVar->FindCommand("ping")->m_nFlags |= FCVAR_GAMEDLL_FOR_REMOTE_CLIENTS;
+	FixupCvarFlags();
 
 	// need to initialise these after host_init since they do stuff to preexisting concommands/convars without being client/server specific
 	InitialiseCommandPrint();
--- NorthstarDLL/misccommands.cpp
+++ NorthstarDLL/misccommands.cpp
@@ -4,8 +4,10 @@
 #include "playlist.h"
 #include "r2engine.h"
 #include "r2client.h"
+#include "tier0.h"
 #include "hoststate.h"
 #include "masterserver.h"
+#include "modmanager.h"
 #include "serverauthentication.h"
 #include "squirrel.h"
 
@@ -62,4 +64,79 @@ void AddMiscConCommands()
 
 	// this is a concommand because we make a deferred call to it from another thread
 	RegisterConCommand("ns_end_reauth_and_leave_to_lobby", ConCommand_ns_end_reauth_and_leave_to_lobby, "", FCVAR_NONE);
+}
+
+// fixes up various cvar flags to have more sane values
+void FixupCvarFlags()
+{
+	if (Tier0::CommandLine()->CheckParm("-allowdevcvars"))
+	{
+		// strip hidden and devonly cvar flags
+		int iNumCvarsAltered = 0;
+		for (auto& pair : R2::g_pCVar->DumpToMap())
+		{
+			// strip flags
+			int flags = pair.second->GetFlags();
+			if (flags & FCVAR_DEVELOPMENTONLY)
+			{
+				flags &= ~FCVAR_DEVELOPMENTONLY;
+				iNumCvarsAltered++;
+			}
+
+			if (flags & FCVAR_HIDDEN)
+			{
+				flags &= ~FCVAR_HIDDEN;
+				iNumCvarsAltered++;
+			}
+
+			pair.second->m_nFlags = flags;
+		}
+
+		spdlog::info("Removed {} hidden/devonly cvar flags", iNumCvarsAltered);
+	}
+
+	const std::vector<std::tuple<const char*, uint32_t>> CVAR_FIXUP_ADD_FLAGS = {
+		// system commands (i.e. necessary for proper functionality)
+		// servers need to be able to disconnect
+		{"disconnect", FCVAR_SERVER_CAN_EXECUTE},
+
+		// clients need to be able to run these on server, which will then print the results of them on client
+		{"status", FCVAR_GAMEDLL_FOR_REMOTE_CLIENTS},
+		{"ping", FCVAR_GAMEDLL_FOR_REMOTE_CLIENTS},
+
+		// cheat commands
+		{"give", FCVAR_GAMEDLL_FOR_REMOTE_CLIENTS},
+		{"give_server", FCVAR_GAMEDLL_FOR_REMOTE_CLIENTS},
+		{"givecurrentammo", FCVAR_GAMEDLL_FOR_REMOTE_CLIENTS},
+		{"takecurrentammo", FCVAR_GAMEDLL_FOR_REMOTE_CLIENTS},
+
+		{"switchclass", FCVAR_GAMEDLL_FOR_REMOTE_CLIENTS},
+		{"set", FCVAR_GAMEDLL_FOR_REMOTE_CLIENTS},
+		{"_setClassVarServer", FCVAR_GAMEDLL_FOR_REMOTE_CLIENTS},
+
+		{"ent_create", FCVAR_GAMEDLL_FOR_REMOTE_CLIENTS},
+		{"ent_throw", FCVAR_GAMEDLL_FOR_REMOTE_CLIENTS},
+		{"ent_setname", FCVAR_GAMEDLL_FOR_REMOTE_CLIENTS},
+		{"ent_teleport", FCVAR_GAMEDLL_FOR_REMOTE_CLIENTS},
+		{"ent_remove", FCVAR_GAMEDLL_FOR_REMOTE_CLIENTS},
+		{"ent_remove_all", FCVAR_GAMEDLL_FOR_REMOTE_CLIENTS},
+		{"ent_fire", FCVAR_GAMEDLL_FOR_REMOTE_CLIENTS},
+
+		{"particle_create", FCVAR_GAMEDLL_FOR_REMOTE_CLIENTS},
+		{"particle_recreate", FCVAR_GAMEDLL_FOR_REMOTE_CLIENTS},
+		{"particle_kill", FCVAR_GAMEDLL_FOR_REMOTE_CLIENTS},
+
+		{"test_setteam", FCVAR_GAMEDLL_FOR_REMOTE_CLIENTS},
+		{"melee_lunge_ent", FCVAR_GAMEDLL_FOR_REMOTE_CLIENTS}};
+
+	const std::vector<std::tuple<const char*, uint32_t>> CVAR_FIXUP_REMOVE_FLAGS = {
+		// unsure how this command works, not even sure it's used on retail servers, deffo shouldn't be used on northstar
+		{"migrateme", FCVAR_SERVER_CAN_EXECUTE},
+	};
+
+	for (auto& fixup : CVAR_FIXUP_ADD_FLAGS)
+		R2::g_pCVar->FindCommandBase(std::get<0>(fixup))->m_nFlags |= std::get<1>(fixup);
+
+	for (auto& fixup : CVAR_FIXUP_REMOVE_FLAGS)
+		R2::g_pCVar->FindCommandBase(std::get<0>(fixup))->m_nFlags &= ~std::get<1>(fixup);
 }
