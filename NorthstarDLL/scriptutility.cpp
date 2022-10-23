2348d86dea6b1159421b6d16d9c43d1d2e3ec2f1
RoyalBlue1 <realEmail@veryRealURL.com>
2022-10-23 16:54:21 +0200

    HelloGecko

--- NorthstarDLL/scriptutility.cpp
+++ NorthstarDLL/scriptutility.cpp
@@ -2,8 +2,12 @@
 #include "squirrel.h"
 
 // asset function StringToAsset( string assetName )
-
-ADD_SQUIRREL_FUNC("asset",StringToAsset,"string assetName", "converts a given string to an asset",(ScriptContext::UI | ScriptContext::CLIENT | ScriptContext::SERVER))
+ADD_SQUIRREL_FUNC(
+	"asset",
+	StringToAsset,
+	"string assetName",
+	"converts a given string to an asset",
+	(ScriptContext::UI | ScriptContext::CLIENT | ScriptContext::SERVER))
 {
 	g_pSquirrel<context>->pushasset(sqvm, g_pSquirrel<context>->getstring(sqvm, 1), -1);
 	return SQRESULT_NOTNULL;
