9f4b00e8ffdb1d74022e512183762e432ddfdd32
RoyalBlue1 <realEmail@veryRealURL.com>
2022-10-23 17:02:29 +0200

    i hate formatting

--- NorthstarDLL/dllmain.cpp
+++ NorthstarDLL/dllmain.cpp
@@ -8,7 +8,7 @@
 #include "plugins.h"
 #include "version.h"
 #include "pch.h"
-#include "squirrelnativeautobind.h" 
+#include "squirrelnativeautobind.h"
 
 #include "rapidjson/document.h"
 #include "rapidjson/stringbuffer.h"
