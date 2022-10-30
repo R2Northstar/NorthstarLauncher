275513ddb6a0eeb2425730d86541a2462148ed78
ASpoonPlaysGames <66967891+ASpoonPlaysGames@users.noreply.github.com>
2022-10-30 22:11:11 +0000

    fix build failure

--- NorthstarDLL/rpakfilesystem.cpp
+++ NorthstarDLL/rpakfilesystem.cpp
@@ -211,7 +211,7 @@ int, __fastcall, (char* pPath, void* unknownSingleton, int flags, void* pCallbac
 		// sp_<map> rpaks contain tutorial ghost data
 		// sucks to have to load the entire rpak for that but sp was never meant to be done on dedi
 		if (IsDedicatedServer() && (Tier0::CommandLine()->CheckParm("-nopakdedi") ||
-    strncmp(&originalPath[0], "common", 6) && strncmp(&originalPath[0], "sp_", 3))
+		strncmp(&originalPath[0], "common", 6) && strncmp(&originalPath[0], "sp_", 3)))
 		{
 			if (bNeedToFreePakName)
 				delete[] pPath;
