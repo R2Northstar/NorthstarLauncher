2f0d75279cb1bb8c095261518678e6653d7f1b60
wolf109909 <84360921+wolf109909@users.noreply.github.com>
2022-11-23 23:31:11 +0800

    i think i fixed clang format maybe

--- NorthstarDLL/hoststate.cpp
+++ NorthstarDLL/hoststate.cpp
@@ -112,13 +112,14 @@ void, __fastcall, (CHostState* self, double flCurrentTime, float flFrameTime))
 			g_pSquirrel<ScriptContext::UI>->ProcessMessageBuffer();
 
 		if (g_pSquirrel<ScriptContext::CLIENT>->m_pSQVM != nullptr && g_pSquirrel<ScriptContext::CLIENT>->m_pSQVM->sqvm != nullptr)
-			g_pSquirrel<ScriptContext::CLIENT>->ProcessMessageBuffer();
-	}
-
-	if (g_pSquirrel<ScriptContext::SERVER>->m_pSQVM != nullptr &&
-		g_pSquirrel<ScriptContext::SERVER>->m_pSQVM->sqvm != nullptr)
-		g_pSquirrel<ScriptContext::SERVER>->ProcessMessageBuffer();
-}
+ 			g_pSquirrel<ScriptContext::CLIENT>->ProcessMessageBuffer();
+ 	}
+ 
+-	if (g_pSquirrel<ScriptContext::SERVER>->m_pSQVM != nullptr &&
+-		g_pSquirrel<ScriptContext::SERVER>->m_pSQVM->sqvm != nullptr)
++	if (g_pSquirrel<ScriptContext::SERVER>->m_pSQVM != nullptr && g_pSquirrel<ScriptContext::SERVER>->m_pSQVM->sqvm != nullptr)
+ 		g_pSquirrel<ScriptContext::SERVER>->ProcessMessageBuffer();
+ }
 
 ON_DLL_LOAD_RELIESON("engine.dll", HostState, ConVar, (CModule module))
 {
