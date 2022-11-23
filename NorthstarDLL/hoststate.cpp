3ad8190cb0777e14e74e049b783f9eaca15dd565
wolf109909 <84360921+wolf109909@users.noreply.github.com>
2022-11-23 23:22:31 +0800

    ok it should compile now

--- NorthstarDLL/hoststate.cpp
+++ NorthstarDLL/hoststate.cpp
@@ -8,7 +8,7 @@
 #include "r2engine.h"
 #include "limits.h"
 #include "squirrel.h"
-
+#include "dedicated.h"
 AUTOHOOK_INIT()
 
 using namespace R2;
@@ -114,8 +114,9 @@ void, __fastcall, (CHostState* self, double flCurrentTime, float flFrameTime))
 		if (g_pSquirrel<ScriptContext::CLIENT>->m_pSQVM != nullptr && g_pSquirrel<ScriptContext::CLIENT>->m_pSQVM->sqvm != nullptr)
 			g_pSquirrel<ScriptContext::CLIENT>->ProcessMessageBuffer();
 	}
-	
-	if (g_pSquirrel<ScriptContext::SERVER>->m_pSQVM != nullptr && g_pSquirrel<ScriptContext::SERVER>->m_pSQVM->sqvm != nullptr)
+
+	if (g_pSquirrel<ScriptContext::SERVER>->m_pSQVM != nullptr &&
+		g_pSquirrel<ScriptContext::SERVER>->m_pSQVM->sqvm != nullptr)
 		g_pSquirrel<ScriptContext::SERVER>->ProcessMessageBuffer();
 }
 
