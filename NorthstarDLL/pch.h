8a295ee6f01032b308614668a0da1bb8c3d5be49
Emma Miler <27428383+emma-miler@users.noreply.github.com>
2022-12-02 21:09:01 +0100

    Remove out

--- NorthstarDLL/pch.h
+++ NorthstarDLL/pch.h
@@ -21,10 +21,7 @@
 namespace fs = std::filesystem;
 
 // clang-format off
-#define assert_msg(expression, msg) (void)(                                                       \
-            (!!(expression)) ||                                                              \
-            (_wassert(_CRT_WIDE(__STR(CONCAT3(expression, - , msg))), _CRT_WIDE(__FILE__), (unsigned)(__LINE__)), 0) \
-        )
+#define assert_msg(expression, msg) (void)((assert((!!(expression)(_CRT_WIDE(__STR(CONCAT3(expression, - , msg))), _CRT_WIDE(__FILE__), (unsigned)(__LINE__)), 0))))
 //clang-format on
 
 #include "structs.h"
