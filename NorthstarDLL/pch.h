d0882a711e3e95f3a77622219c330c31ceaac3ec
Emma Miler <27428383+emma-miler@users.noreply.github.com>
2022-12-11 18:26:10 +0100

    Restructuring

--- NorthstarDLL/pch.h
+++ NorthstarDLL/pch.h
@@ -9,7 +9,7 @@
 #define RAPIDJSON_HAS_STDSTRING 1
 
 // add headers that you want to pre-compile here
-#include "memalloc.h"
+#include "util/memalloc.h"
 
 #include <windows.h>
 #include <psapi.h>
@@ -20,13 +20,17 @@
 
 namespace fs = std::filesystem;
 
-#include "structs.h"
-#include "color.h"
+// clang-format off
+#define assert_msg(exp, msg) assert((exp, msg))
+//clang-format on
+
+#include "util/structs.h"
+#include "math/color.h"
 #include "spdlog/spdlog.h"
-#include "logging.h"
+#include "console/logging.h"
 #include "MinHook.h"
 #include "libcurl/include/curl/curl.h"
-#include "hooks.h"
+#include "hooks/hooks.h"
 #include "memory.h"
 
 template <typename ReturnType, typename... Args> ReturnType CallVFunc(int index, void* thisPtr, Args... args)
