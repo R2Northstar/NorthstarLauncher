d389b59538f65dd31b024310ec8223bde00f75ee
Erlite <ys.aameziane@gmail.com>
2022-12-21 12:54:59 +0100

    Add NSDisconnectPlayer() function.

--- NorthstarDLL/scripts/server/miscserverscript.cpp
+++ NorthstarDLL/scripts/server/miscserverscript.cpp
@@ -58,3 +58,39 @@ ADD_SQFUNC("bool", NSIsDedicated, "", "", ScriptContext::SERVER)
 	g_pSquirrel<context>->pushbool(sqvm, IsDedicatedServer());
 	return SQRESULT_NOTNULL;
 }
+
+ADD_SQFUNC("bool", NSDisconnectPlayer, "entity player, string reason", "Disconnects the player from the server with the given reason", ScriptContext::SERVER)
+{
+	const R2::CBasePlayer* pPlayer = g_pSquirrel<context>->getentity<R2::CBasePlayer>(sqvm, 1);
+	const char* reason = g_pSquirrel<context>->getstring(sqvm, 2);
+
+	if (!pPlayer)
+	{
+		spdlog::warn("Attempted to call NSDisconnectPlayer() with null player.");
+
+		g_pSquirrel<context>->pushbool(sqvm, false);
+		return SQRESULT_NOTNULL;
+	}
+
+	// Shouldn't happen but I like sanity checks.
+	R2::CBaseClient* pClient = &R2::g_pClientArray[pPlayer->m_nPlayerIndex - 1];
+	if (!pClient)
+	{
+		spdlog::warn("NSDisconnectPlayer(): player entity has null CBaseClient!");
+
+		g_pSquirrel<context>->pushbool(sqvm, false);
+		return SQRESULT_NOTNULL;
+	}
+
+	if (reason)
+	{
+		R2::CBaseClient__Disconnect(pClient, 1, reason);
+	}
+	else
+	{
+		R2::CBaseClient__Disconnect(pClient, 1, "Disconnected by the server.");
+	}
+
+	g_pSquirrel<context>->pushbool(sqvm, true);
+	return SQRESULT_NOTNULL;
+}
