2c5dd95395faab0462c68e7ec676c97e42967860
Erlite <ys.aameziane@gmail.com>
2022-12-22 20:09:33 +0100

    Add NSGetLocalPlayerUID() function. (#371)
    * Add NSGetLocalPlayerUID() function.
    
    * Format fix.
    
    * All VMS actually why not
    
    * Add missing includes

--- NorthstarDLL/scripts/scriptutility.cpp
+++ NorthstarDLL/scripts/scriptutility.cpp
@@ -1,5 +1,7 @@
 #include "pch.h"
 #include "squirrel/squirrel.h"
+#include "client/r2client.h"
+#include "engine/r2engine.h"
 
 // asset function StringToAsset( string assetName )
 ADD_SQFUNC(
@@ -11,4 +13,17 @@ ADD_SQFUNC(
 {
 	g_pSquirrel<context>->pushasset(sqvm, g_pSquirrel<context>->getstring(sqvm, 1), -1);
 	return SQRESULT_NOTNULL;
+}
+
+// string function NSGetLocalPlayerUID()
+ADD_SQFUNC(
+	"string", NSGetLocalPlayerUID, "", "Returns the local player's uid.", ScriptContext::UI | ScriptContext::CLIENT | ScriptContext::SERVER)
+{
+	if (R2::g_pLocalPlayerUserID)
+	{
+		g_pSquirrel<context>->pushstring(sqvm, R2::g_pLocalPlayerUserID);
+		return SQRESULT_NOTNULL;
+	}
+
+	return SQRESULT_NULL;
 }
