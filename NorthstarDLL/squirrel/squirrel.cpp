b989248b5cf5f50b9e219a4e399afabdfc6ee8b4
ASpoonPlaysGames <66967891+ASpoonPlaysGames@users.noreply.github.com>
2023-01-04 19:33:24 +0000

    change log message and show console on non-fatal ui compile error

--- NorthstarDLL/squirrel/squirrel.cpp
+++ NorthstarDLL/squirrel/squirrel.cpp
@@ -376,14 +376,14 @@ void __fastcall ScriptCompileErrorHook(HSquirrelVM* sqvm, const char* error, con
 		// kill dedicated server if we hit this
 		if (IsDedicatedServer())
 		{
-			logger->error("Exiting dedicated server due to fatal compile error");
+			logger->error("Exiting dedicated server, compile error is fatal");
 			// flush the logger before we exit so debug things get saved to log file
 			logger->flush();
 			exit(EXIT_FAILURE);
 		}
 		else
 		{
-			logger->error("Disconnecting due to fatal compile error");
+			logger->error("Disconnecting, compile error is fatal");
 
 			R2::Cbuf_AddText(
 				R2::Cbuf_GetCurrentPlayer(),
@@ -400,7 +400,13 @@ void __fastcall ScriptCompileErrorHook(HSquirrelVM* sqvm, const char* error, con
 	}
 	else
 	{
-		logger->warn("Ignoring non-fatal compile error");
+		logger->warn("Not disconnecting, compile error is non-fatal");
+
+		// likely temp: show console so user can see any errors, as error message wont display if ui is dead
+		// maybe we could disable all mods other than the coremods and try a reload before doing this?
+		// could also maybe do some vgui bullshit to show something visually rather than console
+		if (realContext == ScriptContext::UI)
+			R2::Cbuf_AddText(R2::Cbuf_GetCurrentPlayer(), "showconsole", R2::cmd_source_t::kCommandSrcCode);
 	}
 
 	// dont call the original function since it kills game lol
