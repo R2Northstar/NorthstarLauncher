71e761c91a5c1e24975b35dd12a292c99b7599b3
BobTheBob9 <for.oliver.kirkham@gmail.com>
2023-01-13 02:05:59 +0000

    run from cfg/server/ rather than cfg/

--- NorthstarDLL/engine/hoststate.cpp
+++ NorthstarDLL/engine/hoststate.cpp
@@ -34,13 +34,13 @@ void ServerStartingOrChangingMap()
 	memset(commandBuf, 0, sizeof(commandBuf));
 	CCommand tempCommand = *(CCommand*)&commandBuf;
 	if (sLastMode.length() &&
-		CCommand__Tokenize(tempCommand, fmt::format("exec cleanup_gamemode_{}", sLastMode).c_str(), R2::cmd_source_t::kCommandSrcCode))
+		CCommand__Tokenize(tempCommand, fmt::format("exec server/cleanup_gamemode_{}", sLastMode).c_str(), R2::cmd_source_t::kCommandSrcCode))
 		_Cmd_Exec_f(tempCommand, false, false);
 
 	memset(commandBuf, 0, sizeof(commandBuf));
 	if (CCommand__Tokenize(
 			tempCommand,
-			fmt::format("exec setup_gamemode_{}", sLastMode = Cvar_mp_gamemode->GetString()).c_str(),
+			fmt::format("exec server/setup_gamemode_{}", sLastMode = Cvar_mp_gamemode->GetString()).c_str(),
 			R2::cmd_source_t::kCommandSrcCode))
 	{
 		_Cmd_Exec_f(tempCommand, false, false);
@@ -151,7 +151,7 @@ void, __fastcall, (CHostState* self))
 		char* commandBuf[1040]; // assumedly this is the size of CCommand since we don't have an actual constructor
 		memset(commandBuf, 0, sizeof(commandBuf));
 		CCommand tempCommand = *(CCommand*)&commandBuf;
-		if (CCommand__Tokenize(tempCommand, fmt::format("exec cleanup_gamemode_{}", sLastMode).c_str(), R2::cmd_source_t::kCommandSrcCode))
+		if (CCommand__Tokenize(tempCommand, fmt::format("exec server/cleanup_gamemode_{}", sLastMode).c_str(), R2::cmd_source_t::kCommandSrcCode))
 		{
 			_Cmd_Exec_f(tempCommand, false, false);
 			Cbuf_Execute();
