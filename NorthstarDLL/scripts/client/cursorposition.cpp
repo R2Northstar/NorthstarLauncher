c725a2e9bb6ef22dfe3df9c723158c7d0639474a
uniboi <kami.0.katze@gmail.com>
2023-01-21 10:59:21 +0100

    clamp position to screen size

--- NorthstarDLL/scripts/client/cursorposition.cpp
+++ NorthstarDLL/scripts/client/cursorposition.cpp
@@ -4,13 +4,19 @@
 
 ADD_SQFUNC("vector ornull", NSGetCursorPosition, "", "", ScriptContext::UI)
 {
+	RECT rcClient;
 	POINT p;
-	if (GetCursorPos(&p) && ScreenToClient(*g_gameHWND, &p))
+	if (GetCursorPos(&p) && ScreenToClient(*g_gameHWND, &p) && GetClientRect(*g_gameHWND, &rcClient))
 	{
+		std::cout << rcClient.right << " : " << rcClient.bottom << "\n";
 		if (GetAncestor(GetForegroundWindow(), GA_ROOTOWNER) != *g_gameHWND)
 			return SQRESULT_NULL;
 
-		g_pSquirrel<context>->pushvector(sqvm, {(float)p.x, (float)p.y, 0});
+		g_pSquirrel<context>->pushvector(
+			sqvm,
+			{p.x > 0 ? p.x > rcClient.right ? rcClient.right : (float)p.x : 0,
+			 p.y > 0 ? p.y > rcClient.bottom ? rcClient.bottom : (float)p.y : 0,
+			 0});
 		return SQRESULT_NOTNULL;
 	}
 	g_pSquirrel<context>->raiseerror(sqvm, "Failed retrieving cursor position of game window");
