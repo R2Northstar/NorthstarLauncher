1340ad11b745b6de272440ddadab03cc5682aac3
unknown <for.oliver.kirkham@gmail.com>
2023-02-02 00:40:47 +0000

    add CGlobals class and g_pGlobals, and update scripts to support

--- NorthstarDLL/engine/runframe.cpp
+++ NorthstarDLL/engine/runframe.cpp
@@ -5,6 +5,7 @@
 #include "server/serverpresence.h"
 
 AUTOHOOK_INIT()
+
 // clang-format off
 AUTOHOOK(CEngine__Frame, engine.dll + 0x1C8650,
 void, __fastcall, (R2::CEngine* self))
@@ -12,6 +13,7 @@ void, __fastcall, (R2::CEngine* self))
 {
 	CEngine__Frame(self);
 }
+
 ON_DLL_LOAD("engine.dll", RunFrame, (CModule module))
 {
 	AUTOHOOK_DISPATCH()
