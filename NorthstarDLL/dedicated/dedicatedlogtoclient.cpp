b1bfee8ce7a7ba3fb8c5493f73c0fe4700e0433c
unknown <for.oliver.kirkham@gmail.com>
2023-02-10 14:26:42 +0000

    update formatting

--- NorthstarDLL/dedicated/dedicatedlogtoclient.cpp
+++ NorthstarDLL/dedicated/dedicatedlogtoclient.cpp
@@ -2,7 +2,7 @@
 #include "dedicatedlogtoclient.h"
 #include "engine/r2engine.h"
 
-void(*CGameClient__ClientPrintf)(R2::CBaseClient* pClient, const char* fmt, ...);
+void (*CGameClient__ClientPrintf)(R2::CBaseClient* pClient, const char* fmt, ...);
 
 void DedicatedServerLogToClientSink::custom_sink_it_(const custom_log_msg& msg)
 {
@@ -20,7 +20,7 @@ void DedicatedServerLogToClientSink::custom_sink_it_(const custom_log_msg& msg)
 	eSendPrintsToClient eSendPrints = static_cast<eSendPrintsToClient>(Cvar_dedi_sendPrintsToClient->GetInt());
 	if (eSendPrints == eSendPrintsToClient::NONE)
 		return;
-	
+
 	std::string sLogMessage = fmt::format("[DEDICATED SERVER] [{}] {}", level_names[msg.level], msg.payload);
 	for (int i = 0; i < R2::g_pGlobals->m_nMaxClients; i++)
 	{
@@ -45,5 +45,5 @@ void DedicatedServerLogToClientSink::flush_() {}
 
 ON_DLL_LOAD_DEDI("engine.dll", DedicatedServerLogToClient, (CModule module))
 {
-	CGameClient__ClientPrintf = module.Offset(0x1016A0).As<void(*)(R2::CBaseClient*, const char*, ...)>();
+	CGameClient__ClientPrintf = module.Offset(0x1016A0).As<void (*)(R2::CBaseClient*, const char*, ...)>();
 }
