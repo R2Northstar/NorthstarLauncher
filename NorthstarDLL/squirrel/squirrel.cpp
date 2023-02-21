7e3b70f0e5ef5dd513f5d2037ccbfec65e618f90
uniboi <plurals@autism.exposed>
2023-02-21 18:14:30 +0100

    format files

--- NorthstarDLL/squirrel/squirrel.cpp
+++ NorthstarDLL/squirrel/squirrel.cpp
@@ -205,7 +205,6 @@ template <ScriptContext context> void SquirrelManager<context>::VMCreated(CSquir
 				size_t last = 0;
 				size_t next = 0;
 
-
 				size_t rc = v.find('-');
 				if (rc != std::string::npos)
 				{
