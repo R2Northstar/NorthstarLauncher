35e91aa12002f089f8624b63421f370f17e3f4c7
pg9182 <96569817+pg9182@users.noreply.github.com>
2023-03-05 18:43:07 -0500

    Implement Atlas sigreq1 connectionless packet
    Consists of a JSON object including a type key and a HMAC-SHA256
    signature using the gameserver-specific token from the masterserver
    as the key.

--- NorthstarDLL/masterserver/masterserver.h
+++ NorthstarDLL/masterserver/masterserver.h
@@ -126,6 +126,7 @@ class MasterServerManager
 	void AuthenticateWithOwnServer(const char* uid, const char* playerToken);
 	void AuthenticateWithServer(const char* uid, const char* playerToken, RemoteServerInfo server, const char* password);
 	void WritePlayerPersistentData(const char* playerId, const char* pdata, size_t pdataSize);
+	void ProcessConnectionlessPacketSigreq1(std::string req);
 };
 
 extern MasterServerManager* g_pMasterServerManager;
