4af35cc3f9caf5dad7e2fbe672052f70bbadd9f9
BobTheBob <for.oliver.kirkham@gmail.com>
2023-03-06 02:55:37 +0000

    add (experimental) potentially less jank patch

--- NorthstarDLL/server/alltalk.cpp
+++ NorthstarDLL/server/alltalk.cpp
@@ -1,21 +1,27 @@
 #include "core/convar/convar.h"
+#include "engine/r2engine.h"
 
-// this replaces a string compare func
-size_t __fastcall ShouldAllowAlltalk(const char* pMapName, const char* pDesiredMapName)
+size_t __fastcall ShouldAllowAlltalk()
 {
-	// in vanilla, this just does strcmp(g_pMapName, "mp_lobby") and doesn't respect sv_alltalk
+	// this needs to return a 64 bit integer where 0 = true and 1 = false
+
 	static ConVar* Cvar_sv_alltalk = R2::g_pCVar->FindVar("sv_alltalk");
 	if (Cvar_sv_alltalk->GetBool())
 		return 0;
 
-	return strcmp(pMapName, pDesiredMapName);
+	// lobby should default to alltalk, otherwise don't allow it
+	return strcmp(R2::g_pGlobals->m_pMapName, "mp_lobby");
 }
 
 ON_DLL_LOAD_RELIESON("engine.dll", ServerAllTalk, ConVar, (CModule module))
 {
-	// replace strcmp function called in CClient::ProcessVoiceData with ShouldAllowAlltalk()
-	MemoryAddress base = module.Offset(0x108608);
-	// call ShouldAllowAlltalk
-	MemoryAddress writeAddress((uint8_t*)&ShouldAllowAlltalk - module.Offset(0x10860D).m_nAddress);
-	base.Offset(1).Patch((BYTE*)&writeAddress, 4);
+	// replace strcmp function called in CClient::ProcessVoiceData with our own code that calls ShouldAllowAllTalk
+	MemoryAddress base = module.Offset(0x1085FA);
+
+	base.Patch("48 4B"); // mov rax, 64 bit int
+	base.Offset(0x2).Patch((uint8_t*)&ShouldAllowAlltalk, sizeof(uintptr_t));
+	base.Offset(0xA).Patch("FF D0"); // call rax
+
+	// nop until compare (test eax, eax)
+	base.Offset(0xC).NOP(0x7);
 }
