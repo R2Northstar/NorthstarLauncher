3f61ed42c96ff78a1e171a027fbe57313ec1363d
F1F7Y <filip.bartos07@proton.me>
2023-06-24 19:18:11 +0200

    Implement simple timer class

---
+++ NorthstarDLL/util/timer.h
@@ -0,0 +1,32 @@
+#pragma once
+
+#include <chrono>
+
+//-----------------------------------------------------------------------------
+// Purpose: Timer class to help keep track of time
+//-----------------------------------------------------------------------------
+class CTimer
+{
+  public:
+	void Start(void);
+	int GetDuration(void) const;
+
+  private:
+	std::chrono::time_point<std::chrono::steady_clock> m_Clock;
+};
+
+//-----------------------------------------------------------------------------
+// Purpose: 
+//-----------------------------------------------------------------------------
+inline void CTimer::Start(void)
+{
+	m_Clock = std::chrono::steady_clock::now();
+}
+
+//-----------------------------------------------------------------------------
+// Purpose: Returns time passed since Start was called in milliseconds
+//-----------------------------------------------------------------------------
+inline int CTimer::GetDuration(void) const
+{
+	return std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::steady_clock::now() - m_Clock).count();
+}
