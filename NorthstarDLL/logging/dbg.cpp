b0aceda88b94b8c5f586fb56a49dd101b771b84b
F1F7Y <filip.bartos07@proton.me>
2023-07-02 17:21:32 +0200

    Add basic logging functions

---
+++ NorthstarDLL/logging/dbg.cpp
@@ -0,0 +1,16 @@
+#include "dbg.h"
+
+void DevMsg(eLog eContext, const char* fmt, ...)
+{
+
+}
+
+void Warning(eLog eContext, const char* fmt, ...)
+{
+
+}
+
+void Error(eLog eContext, int nCode, const char* fmt, ...)
+{
+
+}
