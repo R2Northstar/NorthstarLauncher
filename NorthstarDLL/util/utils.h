8dbfcb98fb03fcb9a76c94a988a6e7139b76f12a
F1F7Y <filip.bartos07@proton.me>
2023-07-08 23:25:44 +0200

    Formatting

--- NorthstarDLL/util/utils.h
+++ NorthstarDLL/util/utils.h
@@ -5,4 +5,4 @@ namespace NS::Utils
 	void RemoveAsciiControlSequences(char* str, bool allow_color_codes);
 	std::string FormatV(const char* fmt, va_list vArgs);
 	std::string Format(const char* fmt, ...);
-}
+} // namespace NS::Utils
