75756c6376ee3dbec62f995fbbd71722ad8747b5
F1F7Y <filip.bartos07@proton.me>
2023-07-25 18:43:25 +0200

    Format

--- NorthstarDLL/scripts/scriptjson.h
+++ NorthstarDLL/scripts/scriptjson.h
@@ -5,10 +5,11 @@
 #include "rapidjson/writer.h"
 #include "rapidjson/stringbuffer.h"
 
-template <ScriptContext context> void EncodeJSONTable(
+template <ScriptContext context>
+void EncodeJSONTable(
 	SQTable* table,
 	rapidjson::GenericValue<rapidjson::UTF8<char>, rapidjson::MemoryPoolAllocator<SourceAllocator>>* obj,
 	rapidjson::MemoryPoolAllocator<SourceAllocator>& allocator);
 
-template <ScriptContext context> void
-DecodeJsonTable(HSquirrelVM* sqvm, rapidjson::GenericValue<rapidjson::UTF8<char>, rapidjson::MemoryPoolAllocator<SourceAllocator>>* obj);
+template <ScriptContext context>
+void DecodeJsonTable(HSquirrelVM* sqvm, rapidjson::GenericValue<rapidjson::UTF8<char>, rapidjson::MemoryPoolAllocator<SourceAllocator>>* obj);
