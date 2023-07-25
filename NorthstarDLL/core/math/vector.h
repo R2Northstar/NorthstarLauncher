ddb4670354a5f29e6ebd08f625edb2576de700e6
Jan <sentrycraft123@gmail.com>
2023-07-25 16:31:02 +0200

    Remove Vector3 Constructor (#494)
    - Packing was enabled because the compiler may add padding which we don't want.
    - Constructors were removed because they were either rather primitive or broken (the `pRawFloats` one).
    - Use of constructor was replaced with a simple cast, which works just as well.

--- NorthstarDLL/core/math/vector.h
+++ NorthstarDLL/core/math/vector.h
@@ -13,13 +13,6 @@ union Vector3
 
 	float raw[3];
 
-	Vector3() : x(0), y(0), z(0) {}
-	Vector3(float x, float y, float z) : x(x), y(y), z(z) {}
-	Vector3(float* pRawFloats) // assumes float[3] => vector
-	{
-		memcpy(raw, pRawFloats, sizeof(this));
-	}
-
 	void MakeValid()
 	{
 		for (auto& fl : raw)
