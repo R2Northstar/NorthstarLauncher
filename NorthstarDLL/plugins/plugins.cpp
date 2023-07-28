R2Northstar/NorthstarLauncher

commit a65cbeacfbaed17f0526fb513f49338f1151d247

Add support for loading plugins from Thunderstore packages (#513)
Adds support for loading plugins from `packages` directory which was missing from the original PR that introduced the `packages` directory.

NorthstarDLL/plugins/plugins.cpp
@@ -188,8 +188,36 @@ std::optional<Plugin> PluginManager::LoadPlugin(fs::path path, PluginInitFuncs* 
 	return plugin;
 }
 
+inline void FindPlugins(fs::path pluginPath, std::vector<fs::path>& paths)
+{
+	// ensure dirs exist
+	if (!fs::exists(pluginPath) || !fs::is_directory(pluginPath))
+	{
+		return;
+	}
+
+	fs::recursive_directory_iterator iterator(pluginPath);
+	// ensure iterator is not empty
+	if (std::filesystem::begin(iterator) != std::filesystem::end(iterator))
+	{
+		return;
+	}
+
+	for (auto const& entry : iterator)
+	{
+		if (fs::is_regular_file(entry) && entry.path().extension() == ".dll")
+			paths.emplace_back(entry.path());
+	}
+}
+
 bool PluginManager::LoadPlugins()
 {
+	if (strstr(GetCommandLineA(), "-noplugins") != NULL)
+	{
+		NS::log::PLUGINSYS->warn("-noplugins detected; skipping loading plugins");
+		return false;
+	}
+
 	std::vector<fs::path> paths;
 
 	pluginPath = GetNorthstarPrefix() + "/plugins";
@@ -207,28 +235,22 @@ bool PluginManager::LoadPlugins()
 	data.version = ns_version.c_str();
 	data.northstarModule = g_NorthstarModule;
 
-	if (strstr(GetCommandLineA(), "-noplugins") != NULL)
+	FindPlugins(pluginPath, paths);
+
+	// Special case for Thunderstore plugin dirs
+
+	for (fs::directory_entry dir : fs::directory_iterator(GetThunderstoreModFolderPath()))
 	{
-		NS::log::PLUGINSYS->warn("-noplugins detected; skipping loading plugins");
-		return false;
+		fs::path pluginDir = dir.path() / "plugins";
+		FindPlugins(pluginDir, paths);
 	}
-	if (!fs::exists(pluginPath))
-	{
-		NS::log::PLUGINSYS->warn("Could not find a plugins directory. Skipped loading plugins");
-		return false;
-	}
-	// ensure dirs exist
-	fs::recursive_directory_iterator iterator(pluginPath);
-	if (std::filesystem::begin(iterator) == std::filesystem::end(iterator))
+
+	if (paths.empty())
 	{
 		NS::log::PLUGINSYS->warn("Could not find any plugins. Skipped loading plugins");
 		return false;
 	}
-	for (auto const& entry : iterator)
-	{
-		if (fs::is_regular_file(entry) && entry.path().extension() == ".dll")
-			paths.emplace_back(entry.path());
-	}
+
 	for (fs::path path : paths)
 	{
 		if (LoadPlugin(path, &funcs, &data))

commit e742eb35ccc4dab4c0b4cff65b5e72a62346aa74

Fix plugins not loading (#522)
Fixes a regression introduced in #513

NorthstarDLL/plugins/plugins.cpp
@@ -196,14 +196,7 @@ inline void FindPlugins(fs::path pluginPath, std::vector<fs::path>& paths)
 		return;
 	}
 
-	fs::recursive_directory_iterator iterator(pluginPath);
-	// ensure iterator is not empty
-	if (std::filesystem::begin(iterator) != std::filesystem::end(iterator))
-	{
-		return;
-	}
-
-	for (auto const& entry : iterator)
+	for (const fs::directory_entry& entry : fs::recursive_directory_iterator(pluginPath))
 	{
 		if (fs::is_regular_file(entry) && entry.path().extension() == ".dll")
 			paths.emplace_back(entry.path());