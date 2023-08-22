053fa83f98dfd0b0bf52df6d74ac047944c384e9
Rémy Raes <contact@remyraes.com>
2023-08-23 01:07:17 +0200

    feat: complete FetchModsListFromAPI implementation

--- NorthstarDLL/mods/autodownload/moddownloader.cpp
+++ NorthstarDLL/mods/autodownload/moddownloader.cpp
@@ -1,27 +1,83 @@
 #include "moddownloader.h"
+#include <rapidjson/fwd.h>
 
 ModDownloader* g_pModDownloader;
 
-ModDownloader::ModDownloader() {}
+ModDownloader::ModDownloader() {
+	spdlog::info("Mod downloaded initialized");
+	modState = {};
+}
+
+size_t write_to_string(void* ptr, size_t size, size_t count, void* stream)
+{
+	((std::string*)stream)->append((char*)ptr, 0, size * count);
+	return size * count;
+}
 
 void ModDownloader::FetchModsListFromAPI()
 {
-	const char* url = MODS_LIST_URL;
-
 	std::thread requestThread(
-		[url]()
+		[this]()
 		{
-		curl_global_init(CURL_GLOBAL_ALL);
-		CURL* easyhandle = curl_easy_init();
-		std::string readBuffer;
+			CURLcode result;
+			CURL* easyhandle;
+			rapidjson::Document verifiedModsJson;
+			std::string url = MODS_LIST_URL;
 
-		curl_easy_setopt(easyhandle, CURLOPT_URL, url);
-		curl_easy_setopt(easyhandle, CURLOPT_VERBOSE, 1L);
-		curl_easy_setopt(easyhandle, CURLOPT_WRITEDATA, &readBuffer);
+			curl_global_init(CURL_GLOBAL_ALL);
+			easyhandle = curl_easy_init();
+			std::string readBuffer;
 
-		curl_easy_perform(easyhandle);
-		std::cout << readBuffer << std::endl;
+			// Fetching mods list from GitHub repository
+			curl_easy_setopt(easyhandle, CURLOPT_CUSTOMREQUEST, "GET");
+			curl_easy_setopt(easyhandle, CURLOPT_TIMEOUT, 30L);
+			curl_easy_setopt(easyhandle, CURLOPT_URL, url.c_str());
+			curl_easy_setopt(easyhandle, CURLOPT_VERBOSE, 1L);
+			curl_easy_setopt(easyhandle, CURLOPT_WRITEDATA, &readBuffer);
+			curl_easy_setopt(easyhandle, CURLOPT_WRITEFUNCTION, write_to_string);
+			result = curl_easy_perform(easyhandle);
+
+			if (result == CURLcode::CURLE_OK)
+			{
+				spdlog::info("Mods list successfully fetched.");
+			}
+			else
+			{
+				spdlog::error("Fetching mods list failed: {}", curl_easy_strerror(result));
+				goto REQUEST_END_CLEANUP;
+			}
+
+			// Load mods list into local state
+			spdlog::info("Loading mods configuration...");
+			verifiedModsJson.Parse(readBuffer);
+			for (auto i = verifiedModsJson.MemberBegin(); i != verifiedModsJson.MemberEnd(); ++i)
+			{
+				std::string name = i->name.GetString();
+				std::string dependency = i->value["DependencyPrefix"].GetString();
+
+				std::vector<VerifiedModVersion> modVersions;
+				rapidjson::Value& versions = i->value["Versions"];
+				assert(versions.IsArray());
+				for (rapidjson::Value::ConstValueIterator itr = versions.Begin(); itr != versions.End(); ++itr)
+				{
+					const rapidjson::Value& attribute = *itr;
+					assert(attribute.IsObject());
+					std::string version = attribute["Version"].GetString();
+					std::string checksum = attribute["Checksum"].GetString();
+					modVersions.push_back({.version = (char*)version.c_str(), .checksum = (char*)checksum.c_str()});
+				}
+
+				VerifiedModDetails modConfig = {.dependencyPrefix = (char*)dependency.c_str(), .versions = modVersions};
+				verifiedMods.insert({name, modConfig});
+				spdlog::info("==> Loaded configuration for mod \"" + name + "\"");
+			}
+
+			spdlog::info("Done loading verified mods list."); 
+
+		REQUEST_END_CLEANUP:
+			curl_easy_cleanup(easyhandle);
 	});
+	requestThread.detach();
 }
 
 void ConCommand_fetch_verified_mods(const CCommand& args)
