07e4c40846279cb48bdc3c0303d19a1f1ba40224
Remy Raes <raes.remy@gmail.com>
2023-08-26 18:00:04 +0200

    build: disable LZMA compression in minizip

--- cmake/Findminizip.cmake
+++ cmake/Findminizip.cmake
@@ -2,6 +2,8 @@
 if(NOT minizip_FOUND)
 	check_init_submodule(${PROJECT_SOURCE_DIR}/thirdparty/minizip)
 
+	set(MZ_LZMA OFF CACHE BOOL "Disable LZMA & XZ compression")
+
 	add_subdirectory(${PROJECT_SOURCE_DIR}/thirdparty/minizip minizip)
 	set(minizip_FOUND 1 PARENT_SCOPE)
 endif()
