f80506a977df136d922384f5efe4cad8dd4e7996
ASpoonPlaysGames <66967891+ASpoonPlaysGames@users.noreply.github.com>
2023-09-03 21:56:40 +0100

    suffer, as i have

--- NorthstarDLL/client/debugoverlay.cpp
+++ NorthstarDLL/client/debugoverlay.cpp
@@ -75,6 +75,13 @@ typedef void (*RenderBoxType)(Vector3 vOrigin, QAngle angles, Vector3 vMins, Vec
 static RenderBoxType RenderBox;
 static RenderBoxType RenderWireframeBox;
 
+typedef bool (*OverlayBase_t__IsDeadType)(OverlayBase_t* a1);
+static OverlayBase_t__IsDeadType OverlayBase_t__IsDead;
+typedef void (*OverlayBase_t__DestroyOverlayType)(OverlayBase_t* a1);
+static OverlayBase_t__DestroyOverlayType OverlayBase_t__DestroyOverlay;
+
+static ConVar* Cvar_enable_debug_overlays;
+
 // clang-format off
 AUTOHOOK(DrawOverlay, engine.dll + 0xABCB0, 
 void, __fastcall, (OverlayBase_t * pOverlay))
@@ -126,6 +133,73 @@ void, __fastcall, (OverlayBase_t * pOverlay))
 	LeaveCriticalSection((LPCRITICAL_SECTION)((char*)sEngineModule + 0x10DB0A38));
 }
 
+// clang-format off
+AUTOHOOK(DrawAllOverlays, engine.dll + 0xAB780, 
+void, __fastcall, (bool bRender))
+// clang-format on
+{
+	EnterCriticalSection((LPCRITICAL_SECTION)((char*)sEngineModule + 0x10DB0A38)); // s_OverlayMutex
+
+	// vanilla checks the convar first, but the smart pistol lines need to be an exception to this rule
+	// so i am doing this check later and will selectively let draws happen regardless of the convar
+	bool debugOverlaysEnabled = Cvar_enable_debug_overlays->GetBool();
+	OverlayBase_t** startOverlay = (OverlayBase_t**)((char*)sEngineModule + 0x10DB0968);
+	OverlayBase_t* currentOverlay = *startOverlay;
+	OverlayBase_t* previousOverlay = nullptr;
+
+	while (currentOverlay != nullptr)
+	{
+		if (OverlayBase_t__IsDead(currentOverlay))
+		{
+			OverlayBase_t* nextOverlay = currentOverlay->m_pNextOverlay;
+
+			if (previousOverlay != nullptr)
+			{
+				previousOverlay->m_pNextOverlay = nextOverlay;
+			}
+			else
+			{
+				*startOverlay = nextOverlay;
+			}
+
+			OverlayBase_t__DestroyOverlay(currentOverlay);
+			currentOverlay = nextOverlay;
+			continue;
+		}
+
+		bool shouldRender;
+
+		if (currentOverlay->m_nCreationTick == -1)
+		{
+			if ((int)currentOverlay->m_pUnk == -1)
+				goto RENDER_OVERLAY;
+
+			// not particularly sure what this is
+			shouldRender = (int)currentOverlay->m_pUnk == *((int*)((char*)sEngineModule + 0x10DB0980));
+		}
+		else
+		{
+			// not particularly sure what this is
+			shouldRender = currentOverlay->m_nCreationTick == *((int*)((char*)sEngineModule + 0x10DB0984));
+		}
+
+		if (shouldRender)
+		{
+RENDER_OVERLAY:
+			// smart pistol's trace lines for some reason use OVERLAY_TRIANGLE not sure why, nothing else that i've found uses it
+			// so I'm going to assume its fine to allow that draw type through enable_debug_overlays
+			if (bRender && (debugOverlaysEnabled || currentOverlay->m_Type == OVERLAY_TRIANGLE))
+				DrawOverlay(currentOverlay);
+		}
+
+NEXT_OVERLAY:
+		previousOverlay = currentOverlay;
+		currentOverlay = currentOverlay->m_pNextOverlay;
+	}
+
+	LeaveCriticalSection((LPCRITICAL_SECTION)((char*)sEngineModule + 0x10DB0A38));
+}
+
 ON_DLL_LOAD_CLIENT_RELIESON("engine.dll", DebugOverlay, ConVar, (CModule module))
 {
 	AUTOHOOK_DISPATCH()
@@ -133,10 +207,14 @@ ON_DLL_LOAD_CLIENT_RELIESON("engine.dll", DebugOverlay, ConVar, (CModule module)
 	RenderLine = module.Offset(0x192A70).RCast<RenderLineType>();
 	RenderBox = module.Offset(0x192520).RCast<RenderBoxType>();
 	RenderWireframeBox = module.Offset(0x193DA0).RCast<RenderBoxType>();
+
+	OverlayBase_t__IsDead = module.Offset(0xACAC0).RCast<OverlayBase_t__IsDeadType>();
+	OverlayBase_t__DestroyOverlay = module.Offset(0xAB680).RCast<OverlayBase_t__DestroyOverlayType>();
+
 	sEngineModule = reinterpret_cast<HMODULE>(module.m_nAddress);
 
 	// not in g_pCVar->FindVar by this point for whatever reason, so have to get from memory
-	ConVar* Cvar_enable_debug_overlays = module.Offset(0x10DB0990).RCast<ConVar*>();
+	Cvar_enable_debug_overlays = module.Offset(0x10DB0990).RCast<ConVar*>();
 	Cvar_enable_debug_overlays->SetValue(false);
 	Cvar_enable_debug_overlays->m_pszDefaultValue = (char*)"0";
 	Cvar_enable_debug_overlays->AddFlags(FCVAR_CHEAT);
