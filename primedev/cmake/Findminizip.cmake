877443b67b9f4c0d1a28bb1b882b4ad8727b0413
GeckoEidechse <gecko.eidechse+git@pm.me>
2024-01-03 23:21:07 +0100

    temp: Commit formatting changes for testing CI

--- primedev/cmake/Findminizip.cmake
+++ primedev/cmake/Findminizip.cmake
@@ -1,16 +1,30 @@
-
 if(NOT minizip_FOUND)
-	check_init_submodule(${PROJECT_SOURCE_DIR}/primedev/thirdparty/minizip)
+    check_init_submodule(${PROJECT_SOURCE_DIR}/primedev/thirdparty/minizip)
 
-	set(MZ_ZLIB ON CACHE BOOL "Enable ZLIB compression, needed for DEFLATE")
-	set(MZ_BZIP2 OFF CACHE BOOL "Disable BZIP2 compression")
-	set(MZ_LZMA OFF CACHE BOOL "Disable LZMA & XZ compression")
-	set(MZ_PKCRYPT OFF CACHE BOOL "Disable PKWARE traditional encryption")
-	set(MZ_WZAES OFF CACHE BOOL "Disable WinZIP AES encryption")
-	set(MZ_ZSTD OFF CACHE BOOL "Disable ZSTD compression")
-	set(MZ_SIGNING OFF CACHE BOOL "Disable zip signing support")
+    set(MZ_ZLIB
+        ON
+        CACHE BOOL "Enable ZLIB compression, needed for DEFLATE")
+    set(MZ_BZIP2
+        OFF
+        CACHE BOOL "Disable BZIP2 compression")
+    set(MZ_LZMA
+        OFF
+        CACHE BOOL "Disable LZMA & XZ compression")
+    set(MZ_PKCRYPT
+        OFF
+        CACHE BOOL "Disable PKWARE traditional encryption")
+    set(MZ_WZAES
+        OFF
+        CACHE BOOL "Disable WinZIP AES encryption")
+    set(MZ_ZSTD
+        OFF
+        CACHE BOOL "Disable ZSTD compression")
+    set(MZ_SIGNING
+        OFF
+        CACHE BOOL "Disable zip signing support")
 
-	add_subdirectory(${PROJECT_SOURCE_DIR}/primedev/thirdparty/minizip minizip)
-	set(minizip_FOUND 1 PARENT_SCOPE)
+    add_subdirectory(${PROJECT_SOURCE_DIR}/primedev/thirdparty/minizip minizip)
+    set(minizip_FOUND
+        1
+        PARENT_SCOPE)
 endif()
-
