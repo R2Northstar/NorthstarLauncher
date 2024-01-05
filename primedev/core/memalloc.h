497f9674d1d75141a94c541b0079391b9d0dbddc
ASpoonPlaysGames <66967891+ASpoonPlaysGames@users.noreply.github.com>
2024-01-05 18:10:46 +0000

    remove unimplemented function

--- primedev/core/memalloc.h
+++ primedev/core/memalloc.h
@@ -8,7 +8,6 @@
 extern "C" void* _malloc_base(size_t size);
 extern "C" void* _calloc_base(size_t const count, size_t const size);
 extern "C" void* _realloc_base(void* block, size_t size);
-extern "C" void* _recalloc_base(void* const block, size_t const count, size_t const size);
 extern "C" void _free_base(void* const block);
 extern "C" char* _strdup_base(const char* src);
 #pragma warning(pop)
