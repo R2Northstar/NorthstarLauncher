62d55561bde1ab0c1109e3a0a131969b1ada6785
ASpoonPlaysGames <66967891+ASpoonPlaysGames@users.noreply.github.com>
2024-01-05 18:10:09 +0000

    disable 4273 because idk how to fix it

--- primedev/core/memalloc.h
+++ primedev/core/memalloc.h
@@ -3,12 +3,15 @@
 #include "rapidjson/document.h"
 // #include "include/rapidjson/allocators.h"
 
+#pragma warning(push)
+#pragma warning(disable : 4273)
 extern "C" void* _malloc_base(size_t size);
 extern "C" void* _calloc_base(size_t const count, size_t const size);
 extern "C" void* _realloc_base(void* block, size_t size);
 extern "C" void* _recalloc_base(void* const block, size_t const count, size_t const size);
 extern "C" void _free_base(void* const block);
 extern "C" char* _strdup_base(const char* src);
+#pragma warning(pop)
 
 void* operator new(size_t n);
 void operator delete(void* p) noexcept;
