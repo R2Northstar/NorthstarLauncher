0b3826038fee036c136dbd8511ca8a1d8b57c16b
ASpoonPlaysGames <66967891+ASpoonPlaysGames@users.noreply.github.com>
2024-02-11 13:08:29 +0000

    appease the formatting gods

--- primedev/cmake/Findsilver-bun.cmake
+++ primedev/cmake/Findsilver-bun.cmake
@@ -1,8 +1,10 @@
-
 if(NOT silver-bun_FOUND)
     check_init_submodule(${PROJECT_SOURCE_DIR}/primedev/thirdparty/silver-bun)
 
     add_subdirectory(${PROJECT_SOURCE_DIR}/primedev/thirdparty/silver-bun silver-bun)
-    set(silver-bun_FOUND 1 PARENT_SCOPE)
+    set(silver-bun_FOUND
+        1
+        PARENT_SCOPE
+        )
 endif()
 
