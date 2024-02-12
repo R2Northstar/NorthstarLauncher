536131347a51bf04a6ccca7f80d96dade1c1fbeb
p0358 <p0358@users.noreply.github.com>
2024-02-12 12:24:27 +0100

    Set thread names for game threads

--- primedev/core/tier0.cpp
+++ primedev/core/tier0.cpp
@@ -1,5 +1,7 @@
 #include "tier0.h"
 
+AUTOHOOK_INIT()
+
 IMemAlloc* g_pMemAllocSingleton = nullptr;
 
 CommandLineType CommandLine;
@@ -18,10 +20,30 @@ void TryCreateGlobalMemAlloc()
 	g_pMemAllocSingleton = CreateGlobalMemAlloc(); // if it already exists, this returns the preexisting IMemAlloc instance
 }
 
+AUTOHOOK_PROCADDRESS(ThreadSetDebugName, tier0.dll, ThreadSetDebugName, void, __fastcall, (HANDLE threadHandle, const char* name))
+{
+	if (threadHandle == 0)
+		threadHandle = GetCurrentThread();
+
+	// need to grab it dynamically as this function was only introduced at some point in Windows 10
+	static decltype(&SetThreadDescription) _SetThreadDescription =
+		CModule("KernelBase.dll").GetExportedFunction("SetThreadDescription").RCast<decltype(&SetThreadDescription)>();
+
+	// TODO: This "method" of "charset conversion" from string to wstring is abhorrent. Change it to a proper one
+	// as soon as Northstar has some helper function to do proper charset conversions.
+	auto tmp = std::string(name);
+	if (_SetThreadDescription)
+		_SetThreadDescription(threadHandle, std::wstring(tmp.begin(), tmp.end()).c_str());
+
+	ThreadSetDebugName(threadHandle, name);
+}
+
 ON_DLL_LOAD("tier0.dll", Tier0GameFuncs, (CModule module))
 {
 	// shouldn't be necessary, but do this just in case
 	TryCreateGlobalMemAlloc();
+
+	AUTOHOOK_DISPATCH()
 
 	// setup tier0 funcs
 	CommandLine = module.GetExportedFunction("CommandLine").RCast<CommandLineType>();
