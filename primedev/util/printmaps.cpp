8fc770ca1e01118955e2c1266a7314bdf5c06c07
ASpoonPlaysGames <66967891+ASpoonPlaysGames@users.noreply.github.com>
2024-03-02 10:56:52 +0000

    formatting

--- primedev/util/printmaps.cpp
+++ primedev/util/printmaps.cpp
@@ -205,7 +205,8 @@ AUTOHOOK(Host_Map_f, engine.dll + 0x15B340, void, __fastcall, (const CCommand& a
 		spdlog::warn("Map load failed: too many arguments provided");
 		return;
 	}
-	else if (args.ArgC() == 2 &&
+	else if (
+		args.ArgC() == 2 &&
 		std::find_if(vMapList.begin(), vMapList.end(), [&](MapVPKInfo map) -> bool { return map.name == args.Arg(1); }) == vMapList.end())
 	{
 		spdlog::warn("Map load failed: {} not found or invalid", args.Arg(1));
