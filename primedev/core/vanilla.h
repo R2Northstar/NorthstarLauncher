abd0bb13ecbd71069be2065d265652e8eddfad74
Will <39478251+VITALISED@users.noreply.github.com>
2024-05-03 06:47:00 +1000

    Add integrity checks for vanilla stuff

--- primedev/core/vanilla.h
+++ primedev/core/vanilla.h
@@ -1,15 +1,25 @@
 #pragma once
 
+#include "engine/r2engine.h"
+
 /// Determines if we are in vanilla-compatibility mode.
-/// In this mode we shouldn't auth with Atlas, which prevents users from joining a
-/// non-trusted server. This means that we can unrestrict client/server commands
-/// as well as various other small changes for compatibility
 class VanillaCompatibility
 {
 public:
 	bool GetVanillaCompatibility()
 	{
-		return (g_pCVar->FindVar("ns_is_northstar_server")->GetBool() == 0);
+		// this usually means something malicious or an outdated server so just disconnect, unless we're insecure, then it's ok
+		if (g_pCVar->FindVar("serverfilter")->GetBool()
+		&& !g_pCVar->FindVar("ns_is_northstar_server")->GetBool()
+		&& !g_pCVar->FindVar("ns_auth_allow_insecure")->GetBool())
+		{
+			Cbuf_AddText(
+				Cbuf_GetCurrentPlayer(),
+				"disconnect \"Validation failed, this server is either outdated or malicious.\"",
+				cmd_source_t::kCommandSrcCode);
+		}
+
+		return g_pCVar->FindVar("ns_is_northstar_server")->GetBool() == 0;
 	}
 };
 
