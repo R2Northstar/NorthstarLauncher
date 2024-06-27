fc087d804464cc6cb12498e171248186eb7b7c26
Jan <sentrycraft123@gmail.com>
2024-06-28 01:01:28 +0200

    Remove unused typedefs from precompiled header (#737)

--- primedev/pch.h
+++ primedev/pch.h
@@ -20,9 +20,6 @@
 
 namespace fs = std::filesystem;
 
-typedef void (*callable)();
-typedef void (*callable_v)(void* v);
-
 // clang-format off
 #define assert_msg(exp, msg) assert((exp, msg))
 //clang-format on
