cfc7c63495ad26ea088364a3f222bead17af9ad4
Remy Raes <raes.remy@gmail.com>
2024-07-28 19:24:00 +0200

    feat: add NSGetModNamesAndVersions method

--- primedev/scripts/client/scriptmodmenuwithversion.cpp
+++ primedev/scripts/client/scriptmodmenuwithversion.cpp
@@ -1,6 +1,28 @@
 #include "mods/modmanager.h"
 #include "squirrel/squirrel.h"
 
+ADD_SQFUNC("array<RequiredModInfo>", NSGetModNamesAndVersions, "", "", ScriptContext::SERVER | ScriptContext::CLIENT | ScriptContext::UI)
+{
+	g_pSquirrel<context>->newarray(sqvm, 0);
+
+	for (Mod& mod : g_pModManager->m_LoadedMods)
+	{
+		g_pSquirrel<context>->pushnewstructinstance(sqvm, 2);
+
+		// name
+		g_pSquirrel<context>->pushstring(sqvm, mod.Name.c_str(), -1);
+		g_pSquirrel<context>->sealstructslot(sqvm, 0);
+
+		// version
+		g_pSquirrel<context>->pushstring(sqvm, mod.Version.c_str(), -1);
+		g_pSquirrel<context>->sealstructslot(sqvm, 1);
+
+		g_pSquirrel<context>->arrayappend(sqvm, -2);
+	}
+
+	return SQRESULT_NOTNULL;
+}
+
 ADD_SQFUNC("void", NSSetModEnabledWithVersion, "string modName, string modVersion, bool enabled", "", ScriptContext::UI)
 {
 	const SQChar* modName = g_pSquirrel<context>->getstring(sqvm, 1);
