0c3709c617726f1f50b3b43266ca613d9ffa318a
Remy Raes <raes.remy@gmail.com>
2024-07-28 21:36:05 +0200

    feat: add NSIsModEnabledWithVersion method

--- primedev/scripts/client/scriptmodmenuwithversion.cpp
+++ primedev/scripts/client/scriptmodmenuwithversion.cpp
@@ -64,3 +64,21 @@ ADD_SQFUNC(
 
 	return SQRESULT_NULL;
 }
+
+ADD_SQFUNC("bool", NSIsModEnabledWithVersion, "string modName, string modVersion", "", ScriptContext::SERVER | ScriptContext::CLIENT | ScriptContext::UI)
+{
+	const SQChar* modName = g_pSquirrel<context>->getstring(sqvm, 1);
+	const SQChar* modVersion = g_pSquirrel<context>->getstring(sqvm, 2);
+
+	// manual lookup, not super performant but eh not a big deal
+	for (Mod& mod : g_pModManager->m_LoadedMods)
+	{
+		if (!mod.Name.compare(modName) && !mod.Version.compare(modVersion))
+		{
+			g_pSquirrel<context>->pushbool(sqvm, mod.m_bEnabled);
+			return SQRESULT_NOTNULL;
+		}
+	}
+
+	return SQRESULT_NULL;
+}
