2124f3126fe027af089014b99cc2ab657923ec19
FourthVolt <87427011+EM4Volts@users.noreply.github.com>
2024-08-09 16:37:49 +0200

    fix postload crash if no valid json

--- primedev/mods/modmanager.cpp
+++ primedev/mods/modmanager.cpp
@@ -873,7 +873,7 @@ void ModManager::LoadMods()
 										  dRpakJson["Preload"].HasMember(pakName) && dRpakJson["Preload"][pakName].IsTrue());
 
 					// postload things
-					if (!bUseRpakJson ||
+					if (bUseRpakJson &&
 						(dRpakJson.HasMember("Postload") && dRpakJson["Postload"].IsObject() && dRpakJson["Postload"].HasMember(pakName)))
 						modPak.m_sLoadAfterPak = dRpakJson["Postload"][pakName].GetString();
 
