70f6180fdce8b7e10206a9a9a72f0ae1c0460e8b
ASpoonPlaysGames <66967891+ASpoonPlaysGames@users.noreply.github.com>
2024-08-26 17:21:17 +0100

    Manually hook BinkOpen

--- primedev/client/clientvideooverrides.cpp
+++ primedev/client/clientvideooverrides.cpp
@@ -2,10 +2,8 @@
 
 AUTOHOOK_INIT()
 
-// clang-format off
-AUTOHOOK_PROCADDRESS(BinkOpen, bink2w64.dll, BinkOpen, 
-void*, __fastcall, (const char* path, uint32_t flags))
-// clang-format on
+static void* (*__fastcall o_pBinkOpen)(const char* path, uint32_t flags) = nullptr;
+static void* __fastcall h_BinkOpen(const char* path, uint32_t flags)
 {
 	std::string filename(fs::path(path).filename().string());
 	spdlog::info("BinkOpen {}", filename);
@@ -25,10 +23,16 @@ void*, __fastcall, (const char* path, uint32_t flags))
 	{
 		// create new path
 		fs::path binkPath(fileOwner->m_ModDirectory / "media" / filename);
-		return BinkOpen(binkPath.string().c_str(), flags);
+		return o_pBinkOpen(binkPath.string().c_str(), flags);
 	}
 	else
-		return BinkOpen(path, flags);
+		return o_pBinkOpen(path, flags);
+}
+
+ON_DLL_LOAD_CLIENT("bink2w64.dll", BinkRead, (CModule module))
+{
+	o_pBinkOpen = module.GetExportedFunction("BinkOpen").RCast<decltype(o_pBinkOpen)>();
+	HookAttach(&(PVOID&)o_pBinkOpen, (PVOID)h_BinkOpen);
 }
 
 ON_DLL_LOAD_CLIENT("engine.dll", BinkVideo, (CModule module))
