56eb8c8899a640e7978d77c7a9bcef4a082fd5dc
ASpoonPlaysGames <66967891+ASpoonPlaysGames@users.noreply.github.com>
2024-08-31 14:12:30 +0100

    Manually hook ServerCreateInterface

--- primedev/core/sourceinterface.cpp
+++ primedev/core/sourceinterface.cpp
@@ -17,12 +17,10 @@ static void* __fastcall h_ClientCreateInterface(const char* pName, const int* pR
 	return ret;
 }
 
-// clang-format off
-AUTOHOOK_PROCADDRESS(ServerCreateInterface, server.dll, CreateInterface, 
-void*, __fastcall, (const char* pName, const int* pReturnCode))
-// clang-format on
+static void*(__fastcall* o_pServerCreateInterface)(const char* pName, const int* pReturnCode) = nullptr;
+static void* __fastcall h_ServerCreateInterface(const char* pName, const int* pReturnCode)
 {
-	void* ret = ServerCreateInterface(pName, pReturnCode);
+	void* ret = o_pServerCreateInterface(pName, pReturnCode);
 	spdlog::info("CreateInterface SERVER {}", pName);
 
 	return ret;
@@ -46,6 +44,11 @@ ON_DLL_LOAD("client.dll", ClientInterface, (CModule module))
 	o_pClientCreateInterface = module.GetExportedFunction("CreateInterface").RCast<decltype(o_pClientCreateInterface)>();
 	HookAttach(&(PVOID&)o_pClientCreateInterface, (PVOID)h_ClientCreateInterface);
 }
-ON_DLL_LOAD("server.dll", ServerInterface, (CModule module)) {AUTOHOOK_DISPATCH_MODULE(server.dll)}
+ON_DLL_LOAD("server.dll", ServerInterface, (CModule module))
+{
+	AUTOHOOK_DISPATCH_MODULE(server.dll)
+	o_pServerCreateInterface = module.GetExportedFunction("CreateInterface").RCast<decltype(o_pServerCreateInterface)>();
+	HookAttach(&(PVOID&)o_pServerCreateInterface, (PVOID)h_ServerCreateInterface);
+}
 ON_DLL_LOAD("engine.dll", EngineInterface, (CModule module)) {AUTOHOOK_DISPATCH_MODULE(engine.dll)}
 // clang-format on
