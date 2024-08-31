516b1b835f93d63c9ce7518303cb6d0067e0c86f
ASpoonPlaysGames <66967891+ASpoonPlaysGames@users.noreply.github.com>
2024-08-31 14:11:17 +0100

    Manually hook ClientCreateInterface

--- primedev/core/sourceinterface.cpp
+++ primedev/core/sourceinterface.cpp
@@ -5,12 +5,10 @@ AUTOHOOK_INIT()
 
 // really wanted to do a modular callback system here but honestly couldn't be bothered so hardcoding stuff for now: todo later
 
-// clang-format off
-AUTOHOOK_PROCADDRESS(ClientCreateInterface, client.dll, CreateInterface, 
-void*, __fastcall, (const char* pName, const int* pReturnCode))
-// clang-format on
+static void*(__fastcall* o_pClientCreateInterface)(const char* pName, const int* pReturnCode) = nullptr;
+static void* __fastcall h_ClientCreateInterface(const char* pName, const int* pReturnCode)
 {
-	void* ret = ClientCreateInterface(pName, pReturnCode);
+	void* ret = o_pClientCreateInterface(pName, pReturnCode);
 	spdlog::info("CreateInterface CLIENT {}", pName);
 
 	if (!strcmp(pName, "GameClientExports001"))
@@ -42,7 +40,12 @@ void*, __fastcall, (const char* pName, const int* pReturnCode))
 }
 
 // clang-format off
-ON_DLL_LOAD("client.dll", ClientInterface, (CModule module)) {AUTOHOOK_DISPATCH_MODULE(client.dll)}
+ON_DLL_LOAD("client.dll", ClientInterface, (CModule module))
+{
+	AUTOHOOK_DISPATCH_MODULE(client.dll)
+	o_pClientCreateInterface = module.GetExportedFunction("CreateInterface").RCast<decltype(o_pClientCreateInterface)>();
+	HookAttach(&(PVOID&)o_pClientCreateInterface, (PVOID)h_ClientCreateInterface);
+}
 ON_DLL_LOAD("server.dll", ServerInterface, (CModule module)) {AUTOHOOK_DISPATCH_MODULE(server.dll)}
 ON_DLL_LOAD("engine.dll", EngineInterface, (CModule module)) {AUTOHOOK_DISPATCH_MODULE(engine.dll)}
 // clang-format on
