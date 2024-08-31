bcfebd6be245fab2645b8471f4252353d283f917
ASpoonPlaysGames <66967891+ASpoonPlaysGames@users.noreply.github.com>
2024-08-31 14:36:37 +0100

    Remove AUTOHOOK_INIT and AUTOHOOK_DISPATCH

--- primedev/engine/host.cpp
+++ primedev/engine/host.cpp
@@ -6,8 +6,6 @@
 #include "r2engine.h"
 #include "core/tier0.h"
 
-AUTOHOOK_INIT()
-
 static void(__fastcall* o_pHost_Init)(bool bDedicated) = nullptr;
 static void __fastcall h_Host_Init(bool bDedicated)
 {
@@ -27,8 +25,6 @@ static void __fastcall h_Host_Init(bool bDedicated)
 
 ON_DLL_LOAD("engine.dll", Host_Init, (CModule module))
 {
-	AUTOHOOK_DISPATCH()
-
 	o_pHost_Init = module.Offset(0x155EA0).RCast<decltype(o_pHost_Init)>();
 	HookAttach(&(PVOID&)o_pHost_Init, (PVOID)h_Host_Init);
 }
