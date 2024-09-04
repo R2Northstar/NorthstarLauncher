4706f973a613c814d26dc8ebf3f53cd9a9f25dcb
ASpoonPlaysGames <66967891+ASpoonPlaysGames@users.noreply.github.com>
2024-09-04 18:37:27 +0100

    Remove AUTOHOOK_INIT and AUTOHOOK_DISPATCH

--- primedev/engine/runframe.cpp
+++ primedev/engine/runframe.cpp
@@ -3,8 +3,6 @@
 #include "hoststate.h"
 #include "server/serverpresence.h"
 
-AUTOHOOK_INIT()
-
 static void(__fastcall* o_pCEngine__Frame)(CEngine* self) = nullptr;
 static void __fastcall h_CEngine__Frame(CEngine* self)
 {
@@ -13,7 +11,6 @@ static void __fastcall h_CEngine__Frame(CEngine* self)
 
 ON_DLL_LOAD("engine.dll", RunFrame, (CModule module))
 {
-	AUTOHOOK_DISPATCH()
 	o_pCEngine__Frame = module.Offset(0x1C8650).RCast<decltype(o_pCEngine__Frame)>();
 	HookAttach(&(PVOID&)o_pCEngine__Frame, (PVOID)h_CEngine__Frame);
 }
