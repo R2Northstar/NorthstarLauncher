d389b7c7fa6517d18cabd0f349eac0b9ff203a5a
Remy Raes <raes.remy@gmail.com>
2024-11-01 01:58:24 +0100

    feat: add NSGetModVersions method

--- primedev/scripts/client/scriptmodmenu.cpp
+++ primedev/scripts/client/scriptmodmenu.cpp
@@ -122,6 +122,24 @@ ADD_SQFUNC("bool", NSIsModRequiredOnClient, "string modName", "", ScriptContext:
 	return SQRESULT_NULL;
 }
 
+ADD_SQFUNC("array<string>", NSGetModVersions, "string modName", "", ScriptContext::SERVER | ScriptContext::CLIENT | ScriptContext::UI)
+{
+	const SQChar* modName = g_pSquirrel<context>->getstring(sqvm, 1);
+	g_pSquirrel<context>->newarray(sqvm, 0);
+
+	// manual lookup, not super performant but eh not a big deal
+	for (Mod& mod : g_pModManager->m_LoadedMods)
+	{
+		if (!mod.Name.compare(modName))
+		{
+			g_pSquirrel<context>->pushstring(sqvm, mod.Version.c_str());
+			g_pSquirrel<context>->arrayappend(sqvm, -2);
+		}
+	}
+
+	return SQRESULT_NOTNULL;
+}
+
 ADD_SQFUNC("void", NSReloadMods, "", "", ScriptContext::UI)
 {
 	NOTE_UNUSED(sqvm);
