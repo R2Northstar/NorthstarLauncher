348f8687d766a2f23fc8b5ef55c14423afae3ea1
Remy Raes <raes.remy@gmail.com>
2024-11-20 19:15:17 +0100

    feat: add NSGetModInformation method

--- primedev/scripts/client/scriptmodmenu.cpp
+++ primedev/scripts/client/scriptmodmenu.cpp
@@ -57,6 +57,68 @@ ADD_SQFUNC("array<ModInfo>", NSGetModsInformation, "", "", ScriptContext::SERVER
 	return SQRESULT_NOTNULL;
 }
 
+ADD_SQFUNC("array<ModInfo>", NSGetModInformation, "string modName", "", ScriptContext::SERVER | ScriptContext::CLIENT | ScriptContext::UI)
+{
+	const SQChar* modName = g_pSquirrel<context>->getstring(sqvm, 1);
+	g_pSquirrel<context>->newarray(sqvm, 0);
+
+	for (Mod& mod : g_pModManager->m_LoadedMods)
+	{
+		if (mod.Name.compare(modName) != 0)
+		{
+			continue;
+		}
+
+		g_pSquirrel<context>->pushnewstructinstance(sqvm, 9);
+
+		// name
+		g_pSquirrel<context>->pushstring(sqvm, mod.Name.c_str(), -1);
+		g_pSquirrel<context>->sealstructslot(sqvm, 0);
+
+		// description
+		g_pSquirrel<context>->pushstring(sqvm, mod.Description.c_str(), -1);
+		g_pSquirrel<context>->sealstructslot(sqvm, 1);
+
+		// version
+		g_pSquirrel<context>->pushstring(sqvm, mod.Version.c_str(), -1);
+		g_pSquirrel<context>->sealstructslot(sqvm, 2);
+
+		// download link
+		g_pSquirrel<context>->pushstring(sqvm, mod.DownloadLink.c_str(), -1);
+		g_pSquirrel<context>->sealstructslot(sqvm, 3);
+
+		// load priority
+		g_pSquirrel<context>->pushinteger(sqvm, mod.LoadPriority);
+		g_pSquirrel<context>->sealstructslot(sqvm, 4);
+
+		// enabled
+		g_pSquirrel<context>->pushbool(sqvm, mod.m_bEnabled);
+		g_pSquirrel<context>->sealstructslot(sqvm, 5);
+
+		// required on client
+		g_pSquirrel<context>->pushbool(sqvm, mod.RequiredOnClient);
+		g_pSquirrel<context>->sealstructslot(sqvm, 6);
+
+		// is remote
+		g_pSquirrel<context>->pushbool(sqvm, mod.m_bIsRemote);
+		g_pSquirrel<context>->sealstructslot(sqvm, 7);
+
+		// convars
+		g_pSquirrel<context>->newarray(sqvm);
+		for (ModConVar* cvar : mod.ConVars)
+		{
+			g_pSquirrel<context>->pushstring(sqvm, cvar->Name.c_str());
+			g_pSquirrel<context>->arrayappend(sqvm, -2);
+		}
+		g_pSquirrel<context>->sealstructslot(sqvm, 8);
+
+		// add current object to squirrel array
+		g_pSquirrel<context>->arrayappend(sqvm, -2);
+	}
+
+	return SQRESULT_NOTNULL;
+}
+
 ADD_SQFUNC("array<string>", NSGetModNames, "", "", ScriptContext::SERVER | ScriptContext::CLIENT | ScriptContext::UI)
 {
 	g_pSquirrel<context>->newarray(sqvm, 0);
@@ -99,23 +161,6 @@ ADD_SQFUNC("void", NSSetModEnabled, "string modName, bool enabled", "", ScriptCo
 		{
 			mod.m_bEnabled = enabled;
 			return SQRESULT_NULL;
-		}
-	}
-
-	return SQRESULT_NULL;
-}
-
-ADD_SQFUNC("bool", NSIsModRequiredOnClient, "string modName", "", ScriptContext::SERVER | ScriptContext::CLIENT | ScriptContext::UI)
-{
-	const SQChar* modName = g_pSquirrel<context>->getstring(sqvm, 1);
-
-	// manual lookup, not super performant but eh not a big deal
-	for (Mod& mod : g_pModManager->m_LoadedMods)
-	{
-		if (!mod.Name.compare(modName))
-		{
-			g_pSquirrel<context>->pushbool(sqvm, mod.RequiredOnClient);
-			return SQRESULT_NOTNULL;
 		}
 	}
 
