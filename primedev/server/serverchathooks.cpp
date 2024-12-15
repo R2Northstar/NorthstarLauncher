0ba2d2bb83d4ad3dde4fdd88b248b924a165f05d
ASpoonPlaysGames <66967891+ASpoonPlaysGames@users.noreply.github.com>
2024-12-15 17:58:37 +0000

    server: Manually hook CServerGameDLL__OnReceivedSayTextMessage

--- primedev/server/serverchathooks.cpp
+++ primedev/server/serverchathooks.cpp
@@ -19,9 +19,6 @@ class CRecipientFilter
 
 CServerGameDLL* g_pServerGameDLL;
 
-void(__fastcall* CServerGameDLL__OnReceivedSayTextMessage)(
-	CServerGameDLL* self, unsigned int senderPlayerId, const char* text, int channelId);
-
 void(__fastcall* CRecipientFilter__Construct)(CRecipientFilter* self);
 void(__fastcall* CRecipientFilter__Destruct)(CRecipientFilter* self);
 void(__fastcall* CRecipientFilter__AddAllPlayers)(CRecipientFilter* self);
@@ -35,10 +32,9 @@ void(__fastcall* MessageWriteString)(const char* sz);
 void(__fastcall* MessageWriteBool)(bool bValue);
 
 bool bShouldCallSayTextHook = false;
-// clang-format off
-AUTOHOOK(_CServerGameDLL__OnReceivedSayTextMessage, server.dll + 0x1595C0,
-void, __fastcall, (CServerGameDLL* self, unsigned int senderPlayerId, const char* text, bool isTeam))
-// clang-format on
+
+static void (__fastcall* o_pCServerGameDLL__OnReceivedSayTextMessage)(CServerGameDLL* self, unsigned int senderPlayerId, const char* text, bool isTeam) = nullptr;
+static void __fastcall h_CServerGameDLL__OnReceivedSayTextMessage(CServerGameDLL* self, unsigned int senderPlayerId, const char* text, bool isTeam)
 {
 	RemoveAsciiControlSequences(const_cast<char*>(text), true);
 
@@ -47,7 +43,7 @@ void, __fastcall, (CServerGameDLL* self, unsigned int senderPlayerId, const char
 	if (bShouldCallSayTextHook)
 	{
 		bShouldCallSayTextHook = false;
-		_CServerGameDLL__OnReceivedSayTextMessage(self, senderPlayerId, text, isTeam);
+		o_pCServerGameDLL__OnReceivedSayTextMessage(self, senderPlayerId, text, isTeam);
 		return;
 	}
 
@@ -59,13 +55,13 @@ void, __fastcall, (CServerGameDLL* self, unsigned int senderPlayerId, const char
 		"CServerGameDLL_ProcessMessageStartThread", static_cast<int>(senderPlayerId) - 1, text, isTeam);
 
 	if (result == SQRESULT_ERROR)
-		_CServerGameDLL__OnReceivedSayTextMessage(self, senderPlayerId, text, isTeam);
+		o_pCServerGameDLL__OnReceivedSayTextMessage(self, senderPlayerId, text, isTeam);
 }
 
 void ChatSendMessage(unsigned int playerIndex, const char* text, bool isTeam)
 {
 	bShouldCallSayTextHook = true;
-	CServerGameDLL__OnReceivedSayTextMessage(
+	o_pCServerGameDLL__OnReceivedSayTextMessage(
 		g_pServerGameDLL,
 		// Ensure the first bit isn't set, since this indicates a custom message
 		(playerIndex + 1) & CUSTOM_MESSAGE_INDEX_MASK,
@@ -158,8 +154,9 @@ ON_DLL_LOAD_RELIESON("server.dll", ServerChatHooks, ServerSquirrel, (CModule mod
 {
 	AUTOHOOK_DISPATCH_MODULE(server.dll)
 
-	CServerGameDLL__OnReceivedSayTextMessage =
-		module.Offset(0x1595C0).RCast<void(__fastcall*)(CServerGameDLL*, unsigned int, const char*, int)>();
+	o_pCServerGameDLL__OnReceivedSayTextMessage = module.Offset(0x1595C0).RCast<decltype(o_pCServerGameDLL__OnReceivedSayTextMessage)>();
+	HookAttach(&(PVOID&)o_pCServerGameDLL__OnReceivedSayTextMessage, (PVOID)h_CServerGameDLL__OnReceivedSayTextMessage);
+
 	CRecipientFilter__Construct = module.Offset(0x1E9440).RCast<void(__fastcall*)(CRecipientFilter*)>();
 	CRecipientFilter__Destruct = module.Offset(0x1E9700).RCast<void(__fastcall*)(CRecipientFilter*)>();
 	CRecipientFilter__AddAllPlayers = module.Offset(0x1E9940).RCast<void(__fastcall*)(CRecipientFilter*)>();
