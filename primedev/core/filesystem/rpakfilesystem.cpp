7c16800842411ef22daee68e370e3cda04a140ba
Remy Raes <contact@remyraes.com>
2025-04-11 16:24:36 +0200

    Fix Null Deref crash
    Co-authored-by: F1F7Y <filip.bartos07@proton.me>

--- primedev/core/filesystem/rpakfilesystem.cpp
+++ primedev/core/filesystem/rpakfilesystem.cpp
@@ -484,6 +484,14 @@ HOOK(OpenFileHook, o_pOpenFile,
 void*, __fastcall, (const char* pPath, void* pCallback))
 // clang-format on
 {
+	// NOTE [Fifty]: For some reason some users are getting pPath as null when
+	//               loading a server, ReadFileAsync uses CreateFileA and checks
+	//               its return value so this is completely safe
+	if (pPath == NULL)
+	{
+		return ReadFileAsync(pPath, pCallback);
+	}
+	
 	fs::path path(pPath);
 	std::string newPath = "";
 	fs::path filename = path.filename();
