28b16a0d26deb211a5718284fc6632ec08b6bd04
GeckoEidechse <gecko.eidechse+git@pm.me>
2025-04-27 01:51:52 +0200

    chore: Revert unrelate whitespace changes

--- primedev/client/r2client.cpp
+++ primedev/client/r2client.cpp
@@ -4,11 +4,10 @@ char* g_pLocalPlayerUserID;
 char* g_pLocalPlayerOriginToken;
 GetBaseLocalClientType GetBaseLocalClient;
 
-
 ON_DLL_LOAD("engine.dll", R2EngineClient, (CModule module))
 {
 	g_pLocalPlayerUserID = module.Offset(0x13F8E688).RCast<char*>();
 	g_pLocalPlayerOriginToken = module.Offset(0x13979C80).RCast<char*>();
-	GetBaseLocalClient = module.Offset(0x78200).RCast<GetBaseLocalClientType>();
 
+	GetBaseLocalClient = module.Offset(0x78200).RCast<GetBaseLocalClientType>();
 }
