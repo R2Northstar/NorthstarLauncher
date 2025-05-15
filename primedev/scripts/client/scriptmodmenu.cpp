6846d046977e0e06861d6057ff088e61308c2f5b
Rémy Raes <contact@remyraes.com>
2025-05-15 16:09:17 +0200

    feat: add mod version parameter to NSSetModEnabled function

--- primedev/scripts/client/scriptmodmenu.cpp
+++ primedev/scripts/client/scriptmodmenu.cpp
@@ -92,15 +92,16 @@ ADD_SQFUNC("array<string>", NSGetModNames, "", "", ScriptContext::SERVER | Scrip
 	return SQRESULT_NOTNULL;
 }
 
-ADD_SQFUNC("void", NSSetModEnabled, "string modName, bool enabled", "", ScriptContext::SERVER | ScriptContext::CLIENT | ScriptContext::UI)
+ADD_SQFUNC("void", NSSetModEnabled, "string modName, string modVersion, bool enabled", "", ScriptContext::SERVER | ScriptContext::CLIENT | ScriptContext::UI)
 {
 	const SQChar* modName = g_pSquirrel<context>->getstring(sqvm, 1);
-	const SQBool enabled = g_pSquirrel<context>->getbool(sqvm, 2);
+	const SQChar* modVersion = g_pSquirrel<context>->getstring(sqvm, 2);
+	const SQBool enabled = g_pSquirrel<context>->getbool(sqvm, 3);
 
 	// manual lookup, not super performant but eh not a big deal
 	for (Mod& mod : g_pModManager->m_LoadedMods)
 	{
-		if (!mod.Name.compare(modName))
+		if (!mod.Name.compare(modName) && !mod.Version.compare(modVersion))
 		{
 			mod.m_bEnabled = enabled;
 			return SQRESULT_NULL;
