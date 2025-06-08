4360e6f0dfd668b7b2a3617430c0bd94911a5ea6
ASpoonPlaysGames <66967891+ASpoonPlaysGames@users.noreply.github.com>
2025-06-08 19:20:07 +0100

    early out of mod load stuff

--- primedev/mods/modmanager.cpp
+++ primedev/mods/modmanager.cpp
@@ -121,6 +121,9 @@ void ModManager::LoadMods()
 
 	for (Mod& mod : m_LoadedMods)
 	{
+		if (!mod.m_bEnabled)
+			continue;
+
 		// register convars
 		// for reloads, this is sorta barebones, when we have a good findconvar method, we could probably reset flags and stuff on
 		// preexisting convars note: we don't delete convars if they already exist because they're used for script stuff, unfortunately this
