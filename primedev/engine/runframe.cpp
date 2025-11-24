4759fc98ad5f56bb6530ed56693d3f2075b6c99f
Allusive <154700875+AllusiveWheat@users.noreply.github.com>
2025-11-23 17:08:33 -0800

    formatting

--- primedev/engine/runframe.cpp
+++ primedev/engine/runframe.cpp
@@ -12,5 +12,5 @@ static void __fastcall h_CEngine__Frame(CEngine* self)
 ON_DLL_LOAD("engine.dll", RunFrame, (CModule module))
 {
 	o_pCEngine__Frame = module.Offset(0x1C8650).RCast<decltype(o_pCEngine__Frame)>();
-	//HookAttach(&(PVOID&)o_pCEngine__Frame, (PVOID)h_CEngine__Frame);
+	// HookAttach(&(PVOID&)o_pCEngine__Frame, (PVOID)h_CEngine__Frame);
 }
