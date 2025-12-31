196fc8d91e9260e3c02b55c3eda73c7c88984368
FourthVolt <87427011+EM4Volts@users.noreply.github.com>
2025-12-31 16:05:58 +0100

    Implement dynamic texture change command

---
+++ primedev/materialsystem/matsyschangedynamicflag.cpp
@@ -0,0 +1,55 @@
+#include <winternl.h>
+#include <d3d11.h>
+
+AUTOHOOK_INIT()
+
+struct DynamicTexture
+{
+	int64_t qword_0[2];
+	ID3D11Resource* texture;
+	ID3D11ShaderResourceView* resourceView;
+	BYTE gap_20[8];
+	int64_t qword_28;
+	ID3D11ShaderResourceView* resourceView2;
+	BYTE gap_[30];
+	uint16_t samplerStateId;
+	WORD word_58;
+	WORD word_5A;
+	BYTE gap_5C[4];
+};
+
+int16_t word_21CB88;
+ID3D11DeviceContext** DeviceContext;
+DynamicTexture*** Src;
+
+int Swap_Texture = 15;
+
+void ConCommand_ChangeDynamicTexture(const CCommand& args)
+{
+	if (args.ArgC() < 2)
+	{
+		spdlog::warn("Usage: ChangeDynamicTexture <texture_id>");
+		return;
+	}
+	int textureId = std::atoi(args.Arg(1));
+	Swap_Texture = textureId;
+	spdlog::info("Swap_Texture set to: {}", Swap_Texture);
+
+}
+
+AUTOHOOK(SUB_25B90, materialsystem_dx11.dll + 0x25B90, void, __fastcall, ())
+{
+	(*DeviceContext)->PSSetShaderResources(20LL, 1LL, &(*Src)[0][Swap_Texture].resourceView);
+	(*DeviceContext)->PSSetShaderResources(21LL, 1LL, &(*Src)[0][41].resourceView);
+}
+
+ON_DLL_LOAD_CLIENT("materialsystem_dx11.dll", SUB_25B90, (CModule module))
+{
+	RegisterConCommand("ChangeDynamicTexture", ConCommand_ChangeDynamicTexture, "mad be sad frfr", FCVAR_GAMEDLL);
+
+	word_21CB88 = *module.Offset(0x21CB88).RCast<int16_t*>();
+	DeviceContext = module.Offset(0x14E8DD8).RCast<ID3D11DeviceContext**>();
+	Src = module.Offset(0x19B13D8).RCast<DynamicTexture***>();
+
+	AUTOHOOK_DISPATCH()
+}
